cebba05c43d381ae74e6e134b651c5692e958b64
boxnos <boxnos@yahoo.com>
2014-06-03 00:31:09 +0900

    reinventing swap()

--- ITP1/ITP1_3_C/main.cpp
+++ ITP1/ITP1_3_C/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+void swap(T* a, T* b)
+{
+    T* tmp = a;
+    a = b;
+    b = tmp;
+}
+
 int main()
 {
-    cin;
-    cout << endl;
+    int a, b;
+
+    while (cin >> a >> b) {
+        if (a == 0 && b == 0)
+            break;
+        if (a > b)
+            swap(a, b);
+        cout << a << " " << b << endl;
+    }
 
     return 0;
 }
