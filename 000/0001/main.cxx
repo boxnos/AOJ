c08043f78a7a540724c37187961d85b1140be8cc
boxnos <boxnos@yahoo.com>
2014-06-05 02:51:51 +0900

    use goto

--- 000/0001/main.cxx
+++ 000/0001/main.cxx
@@ -8,19 +8,21 @@ int main()
     int height;
     vector<int> top3;
 
-    while (cin >> height) {
+loop:
+    if (cin >> height) {
         for (auto it = top3.begin(); it != top3.end(); it++) {
             if (height > *it) {
                 it = top3.insert(it, height);
-                break;
+                if (top3.size() > 3)
+                    top3.pop_back();
+                goto loop;
             }
         }
-        if (top3.size() > 3)
-            top3.pop_back();
-        else if (top3.size() < 3)
+        if (top3.size() < 3)
             top3.push_back(height);
+        goto loop;
     }
-    
+
     for (auto h: top3)
         cout << h << endl;
 
