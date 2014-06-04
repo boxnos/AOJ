667136b4c5f2b132611d13bb1a841e5522aee3f3
boxnos <boxnos@yahoo.com>
2014-06-05 03:14:39 +0900

    use inline function

--- 000/0001/main.cxx
+++ 000/0001/main.cxx
@@ -3,25 +3,27 @@
 #include <iterator>
 using namespace std;
 
+inline void make_top3(vector<int> &top3, int height) {
+    for (auto it = top3.begin(); it != top3.end(); it++) {
+        if (height > *it) {
+            it = top3.insert(it, height);
+            if (top3.size() > 3)
+                top3.pop_back();
+            return;
+        }
+    }
+    if (top3.size() < 3)
+        top3.push_back(height);
+    return;
+}
+
 int main()
 {
     int height;
     vector<int> top3;
 
-loop:
-    if (cin >> height) {
-        for (auto it = top3.begin(); it != top3.end(); it++) {
-            if (height > *it) {
-                it = top3.insert(it, height);
-                if (top3.size() > 3)
-                    top3.pop_back();
-                goto loop;
-            }
-        }
-        if (top3.size() < 3)
-            top3.push_back(height);
-        goto loop;
-    }
+    while (cin >> height)
+        make_top3(top3, height);
 
     for (auto h: top3)
         cout << h << endl;
