49bc20e1f685ea26d18c4468d86313a4d82a2602
boxnos <boxnos@yahoo.com>
2014-06-06 02:35:30 +0900

    accepted 0002 in C++

--- 000/0002/main.cpp
+++ 000/0002/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    int a, b;
+    while(cin >> a >> b) {
+        int sum = a + b;
+        cout << (sum == 0 ? 1 : (int) log10(sum) + 1) << endl;
+    }
 
     return 0;
 }
