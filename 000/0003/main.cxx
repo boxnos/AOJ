7bd0e4669509558efe03b06aabc191236fe9e1fe
boxnos <boxnos@yahoo.com>
2014-06-06 16:03:07 +0900

    accepted in C++11

---
+++ 000/0003/main.cxx
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+inline int square(int x) {
+    return x * x;
+}
+
+bool is_right_triangle(vector<int> &sides)
+{
+    sort(sides.begin(), sides.end());
+    return square(sides[0]) + square(sides[1]) == square(sides[2]);
+}
+
+int main()
+{
+    int length;
+    cin >> length;
+
+    for (vector<int> sides(3); length > 0 && cin >> sides[0] >> sides[1] >> sides[2]; length--)
+        cout << (is_right_triangle(sides) ? "YES" : "NO") << endl;
+
+    return 0;
+}
