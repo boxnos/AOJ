e09598f817d78902f3bbbe0cd1003203927090a3
boxnos <boxnos@yahoo.com>
2014-06-06 15:03:20 +0900

    initial commit for 0003

---
+++ 000/0003/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length;
+    cin >> length;
+
+    for (int a, b, c; length == 0; cin >> a, b, c; length--) {
+        
+    }
+
+    return 0;
+}
