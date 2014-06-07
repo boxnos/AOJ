7888d11b24874cb6dc396c41a3227007ab12ec19
boxnos <boxnos@yahoo.com>
2014-06-07 16:58:45 +0900

    accepted in c++

--- 000/0004/main.cpp
+++ 000/0004/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    for (double a, b, c, d, e, f; cin >> a >> b >> c >> d >> e >> f; )
+        cout << fixed << setprecision(3)
+            << (b*f - c*e)/(b*d - a*e) + 0.0
+            << " "
+            << (a*f - c*d)/(a*e - b*d) + 0.0 << endl;
 
     return 0;
 }
---
+++ 000/0004/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << fixed << setprecision(3) << -0.0 + 0 << endl; // -0.000
+
+    return 0;
+}
