ac4e9a4e54f131e35f6d3cec592f93eda7982093
boxnos <boxnos@yahoo.com>
2014-06-09 04:02:47 +0900

    accepted in c++

--- 000/0007/main.cpp
+++ 000/0007/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+int debt_in_weeks(int weeks)
+{
+    int borrow = 100000;
+    for (;weeks != 0 ; weeks--)
+        borrow = ceil((borrow * 1.05) / 1000) * 1000;
+
+    return borrow;
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    int weeks;
+    cin >> weeks;
+    cout << debt_in_weeks(weeks)<< endl;
 
     return 0;
 }
