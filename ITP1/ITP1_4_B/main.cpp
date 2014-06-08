744f0f5c5b4cf2628414e27f6e16c281d4574670
boxnos <boxnos@yahoo.com>
2014-06-09 02:49:20 +0900

    accepted ITP1_4_B in c++

--- ITP1/ITP1_4_B/main.cpp
+++ ITP1/ITP1_4_B/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    double r;
+    cin >> r;
+    cout << fixed << M_PI * r * r << " " << 2 * M_PI * r << endl;
 
     return 0;
 }
