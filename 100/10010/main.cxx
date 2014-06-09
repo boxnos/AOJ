b2341ad4241f584ee78b039733b8488986a396c5
boxnos <boxnos@yahoo.com>
2014-06-10 01:41:38 +0900

    accepted in c++11

--- 100/10010/main.cxx
+++ 100/10010/main.cxx
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <functional>
 using namespace std;
+
+function<int (int, int)> make_lambda(char &op)
+{
+    switch (op) {
+    case '+': return [](int a, int b) {return a + b;}; break;
+    case '-': return [](int a, int b) {return a - b;}; break;
+    case '*': return [](int a, int b) {return a * b;}; break;
+    case '/': return [](int a, int b) {return a / b;}; break;
+    }
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    int a, b;
+    char op;
+
+    while (cin >> a >> op >> b && op != '?')
+        cout << make_lambda(op)(a, b) << endl;
 
     return 0;
 }
