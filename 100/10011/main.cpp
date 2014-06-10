86b62172d43662bef892ff475e04d6145b476e15
boxnos <boxnos@yahoo.com>
2014-06-11 00:14:17 +0900

    accepted in c++

--- 100/10011/main.cpp
+++ 100/10011/main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <sstream>
 using namespace std;
+
+#define all(bind) bind.begin(), bind.end()
+
+template <typename T>
+string join(vector<T> &v, const string &delimiter)
+{
+    stringstream buffer;
+    if (v.size() > 0) {
+        buffer << v[0];
+        for (typename vector<T>::iterator it = v.begin() + 1; it != v.end(); it++)
+            buffer << delimiter << *it;
+    }
+    return buffer.str();
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    int n;
+    vector<int> a;
+
+    cin >> n;
+    for (int buffer; cin >> buffer && n; n--)
+        a.push_back(buffer);
+
+    reverse(all(a));
+
+    cout << join(a, " ") << endl;
 
     return 0;
 }
