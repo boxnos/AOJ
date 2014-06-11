73c5b807f41771ef44d87e650c35b90db5c31fba
boxnos <boxnos@yahoo.com>
2014-06-12 03:06:49 +0900

    simple sieve

--- 000/0009/main.cpp
+++ 000/0009/main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
+
+void sieve(int n, vector<int> &primes)
+{
+    vector<bool> v(n + 1, true);
+
+    // check
+    for (int i = 2, end = sqrt(n); i <= end; i++) {
+        if (!i)
+            continue;
+        for (int j = i + i; j <= n; j += i)
+            v[j] = false;
+    }
+
+    for (int i = 2; i <= n; i++)
+         if (v[i])
+            primes.push_back(i);
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    vector<int> primes;
+    int i;
+    sieve(100, primes);
+
+    for (int x; cin >> x;) {
+        for (i = 0; i < primes.size(); i++)
+            if (primes[i] == x) {
+                i++;
+                break;
+            } else if (primes[i] > x)
+                break;
+        cout << i << endl;
+    }
 
     return 0;
 }
