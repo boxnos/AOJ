7f86620c9c2c02c5f0e8314a92c8917defe48905
boxnos <boxnos@yahoo.com>
2014-06-11 23:14:57 +0900

    accepted in C++

--- 100/10013/main.cpp
+++ 100/10013/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
+#define times(n, bind) for (int bind = 0; bind < n; bind++)
+
+inline bool is_edge(int &H, int &W, int &x, int &y)
+{
+    return x == 0 || y == 0 || x == W - 1 || y == H - 1;
+}
+
 int main()
 {
-    cin;
-    cout << endl;
+    for (int H, W; cin >> H >> W && H && W;) {
+        times(H, i) {
+            times(W, j)
+                cout << (is_edge(H, W, j, i)? '#' : '.');
+            cout << endl;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
