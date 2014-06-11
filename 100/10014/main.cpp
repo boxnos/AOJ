569d74a9870395b268811785f481240ee53ef2e8
boxnos <boxnos@yahoo.com>
2014-06-12 00:01:15 +0900

    accepted in C++

--- 100/10014/main.cpp
+++ 100/10014/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 using namespace std;
 
+#define times(n, bind) for (int bind = 0; bind < n; bind++)
+
+inline bool is_black(int &x, int &y)
+{
+    return (x & 1) == (y & 1);
+}
+
 int main()
 {
-    cin;
-    cout << endl;
+    for (int H, W; cin >> H >> W && H && W;) {
+        times(H, i) {
+            times(W, j)
+                cout << (is_black(j, i)? '#' : '.');
+            cout << endl;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
