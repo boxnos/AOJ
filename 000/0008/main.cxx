817aff2270505ecec4813e791f67f61dcbd99814
boxnos <boxnos@yahoo.com>
2014-06-14 18:27:24 +0900

    improve readability

--- 000/0008/main.cxx
+++ 000/0008/main.cxx
@@ -1,6 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int count_conbination(int n, int k)
+{
+    if (k == 1)
+        return (n <= 9) ? 1 : 0;
+
+    int count = 0;
+    for (int i = 0, rest; i <= 9; i++) {
+        rest = n - i;
+        if (rest < 0)
+            break;
+        else
+            count += count_conbination(rest, k - 1);
+    }
+
+    return count;
+}
+
+int main()
+{
+    for (int n; cin >> n;)
+        cout << count_conbination(n, 4) << endl;
+
+    return 0;
+}
+
 /*
 inline int factorial(int n)
 {
@@ -28,32 +53,3 @@ inline int homogeneous(int n, int r)
     return conbination(n + r - 1, r);
 }
 */
-
-int count_conbination(int n, int k)
-{
-    int count = 0;
-
-    if (k == 1)
-        if (n <= 9)
-            return 1;
-        else
-            return 0;
-
-    for (int i = 0; i <= 9; i++) {
-        int rest = n - i;
-        if (rest < 0)
-            break;
-        else
-            count += count_conbination(rest, k - 1);
-    }
-
-    return count;
-}
-
-int main()
-{
-    for (int n; cin >> n;)
-        cout << count_conbination(n, 4) << endl;
-
-    return 0;
-}
