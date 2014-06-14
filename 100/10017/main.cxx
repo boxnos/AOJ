fcb957f79cae91fe96a4d119d8c160f2c6079334
boxnos <boxnos@yahoo.com>
2014-06-15 04:57:42 +0900

    accepted in c++11

--- 100/10017/main.cxx
+++ 100/10017/main.cxx
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int how_many_ways(int n, int x, int length, int start)
+{
+    if (length == 0)
+        return (x == 0) ? 1 : 0;
+    else if (start > x)
+        return 0;
+
+    int count = 0;
+
+    for (int i = start; i <= n; i++)
+        count += how_many_ways(n, x - i, length - 1, i + 1);
+
+    return count;
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    for (int n, x; cin >> n >> x && (n || x);)
+        cout << how_many_ways(n, x, 3, 1) << endl;
 
     return 0;
 }
