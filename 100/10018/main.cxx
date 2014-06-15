3770d5f11432ad20525cdb96ace5730260c28bda
boxnos <boxnos@yahoo.com>
2014-06-16 02:20:18 +0900

    bitwise operation version

--- 100/10018/main.cxx
+++ 100/10018/main.cxx
@@ -4,8 +4,8 @@ using namespace std;
 char toggle_case(char c)
 {
     return
-        (0x41 <= c && c <= 0x5A) ? c + 0x20 :
-        (0x61 <= c && c <= 0x7A) ? c - 0x20 : c;
+        (0x41 <= c && c <= 0x5A) ? c | 0x20 :
+        (0x61 <= c && c <= 0x7A) ? c & ~0x20 : c;
 }
 
 int main()
