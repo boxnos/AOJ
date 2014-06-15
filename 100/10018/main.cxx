217e05079f9555c4fdc6b7fe7b4be79a5ad57cbf
boxnos <boxnos@yahoo.com>
2014-06-16 03:16:04 +0900

    set 0x20 to upper

--- 100/10018/main.cxx
+++ 100/10018/main.cxx
@@ -3,7 +3,8 @@ using namespace std;
 
 inline char toggle_case(char c)
 {
-    return (0x41 <= c && c <= 0x5A || 0x61 <= c && c <= 0x7A) ? c ^ ' ' : c;
+    char upper = c | ' ';
+    return (0x61 <= upper && upper <= 0x7A) ? c ^ ' ' : c;
 }
 
 int main()
