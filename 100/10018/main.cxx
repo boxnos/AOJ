6cf57021c2f021c0b074932059fd378b5ec7ed32
boxnos <boxnos@yahoo.com>
2014-06-16 02:09:03 +0900

    ASCII version

--- 100/10018/main.cxx
+++ 100/10018/main.cxx
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+char toggle_case(char c)
+{
+    return
+        (0x41 <= c && c <= 0x5A) ? c + 0x20 :
+        (0x61 <= c && c <= 0x7A) ? c - 0x20 : c;
+}
+
 int main()
 {
-    cin;
-    cout << endl;
+    for (int c; (c = cin.get()) != -1;)
+        cout << toggle_case((char) c);
 
     return 0;
 }
