0d089c29b2e487f5448ce5b87e5c2000e194ef6e
boxnos <boxnos@yahoo.com>
2014-06-17 01:03:21 +0900

    failed version...

--- 000/10019/main.cxx
+++ 000/10019/main.cxx
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
+inline int sum_of_numbers(int x)
+{
+    int sum = 0;
+    for (;x > 0; x /= 10)
+        sum += x % 10;
+    return sum;
+}
+
 int main()
 {
-    cin;
-    cout << endl;
+    for (int x; cin >> x && x;)
+        cout << sum_of_numbers(x) << endl;
 
     return 0;
 }
