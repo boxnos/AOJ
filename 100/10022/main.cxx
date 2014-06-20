77ee18ca482db7ebbefbb989ce4eb5fda76846db
boxnos <boxnos@yahoo.com>
2014-06-20 13:53:35 +0900

    accepted in C++11

--- 100/10022/main.cxx
+++ 100/10022/main.cxx
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
+
+bool find(string a, string b)
+{
+    auto ai = a.begin(), bi = b.begin();
+
+    for (; ai != a.end() || bi != b.end() ; ai++, bi++)
+        if (tolower(*ai) != tolower(*bi))
+            return false;
+
+    return (ai == a.end() && bi == b.end()) ? true : false;
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    string input;
+    int count = 0;
+
+    cin >> input;
+
+    for (string buffer; cin >> buffer && buffer != "END_OF_TEXT";)
+        if (find(input, buffer))
+            count++;
+
+    cout << count << endl;
 
     return 0;
 }
