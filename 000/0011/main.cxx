e3bb40ba1c44515a73f2a183a7fe0ea8c4f58890
boxnos <boxnos@yahoo.com>
2014-06-21 03:15:40 +0900

    accepted in C++11

--- 000/0011/main.cxx
+++ 000/0011/main.cxx
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    int w, n;
+    char c;
+
+    cin >> w >> n;
+
+    list<vector <int>> routes;
+
+    for (vector<int> r(2); n-- && cin >> r[0] >> c >> r[1];)
+        routes.push_front(r);
+
+    for (int i = 1, pos = i; i <= w; pos = ++i) {
+        for (auto r: routes)
+            if (r[0] == pos)
+                pos = r[1];
+            else if(r[1] == pos)
+                pos = r[0];
+
+        cout << pos << endl;
+    }
 
     return 0;
 }
