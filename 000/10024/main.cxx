b8406675c1b945b5dbd5512c707f22eda85c9de7
boxnos <boxnos@yahoo.com>
2014-06-22 01:42:28 +0900

    accepted in C++11

--- 000/10024/main.cxx
+++ 000/10024/main.cxx
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+inline double square(double x) { return x * x; }
+inline double distance2d(double x1, double y1, double x2, double y2)
+{
+    return sqrt(square(x1 - x2) + square(y1 - y2));
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    for (double x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;)
+        cout << fixed << distance2d(x1, y1, x2, y2) << endl;
 
     return 0;
 }
