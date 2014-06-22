2387b01de3c28f71c314ceb9d82bb901d1d1c0dd
boxnos <boxnos@yahoo.com>
2014-06-22 11:55:34 +0900

    enhance readability of operator << and >>

--- 000/0012/main.cxx
+++ 000/0012/main.cxx
@@ -7,28 +7,27 @@ struct Point2D {
     double x, y;
 };
 
-istream &operator>>(istream &is, Point2D &p)
-{
-    is >> p.x >> p.y;
-    return is;
-}
-
-ostream &operator<<(ostream &os, Point2D &p)
-{
-    os << fixed << p.x << " " << p.y;
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, vector<T> &v)
-{
-    if (v.size()) {
-        cout << v.front();
-        for (auto vi = v.begin() + 1; vi != v.end(); vi++)
-            cout << " " << *vi;
+namespace std {
+    istream &operator>>(istream &is, Point2D &p) {
+        is >> p.x >> p.y;
+        return is;
     }
 
-    return os;
+    ostream &operator<<(ostream &os, Point2D &p) {
+        os << fixed << p.x << " " << p.y;
+        return os;
+    }
+
+    template <typename T>
+    ostream &operator<<(ostream &os, vector<T> &v) {
+        if (v.size()) {
+            cout << v.front();
+            for (auto vi = v.begin() + 1; vi != v.end(); vi++)
+                cout << " " << *vi;
+        }
+
+        return os;
+    }
 }
 
 int main()
