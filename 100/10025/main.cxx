022de932864468e8d01bd3701d9c4b89af7c11f2
boxnos <boxnos@yahoo.com>
2014-06-24 01:29:12 +0900

    accepted in C++11

--- 100/10025/main.cxx
+++ 100/10025/main.cxx
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    double a, b, C, rad;
+
+    cin >> a >> b >> C;
+
+    rad = C * M_PI / 180;
+
+    cout << fixed << a * b * sin(rad) / 2 << endl;
+    cout << fixed << a + b + sqrt(pow(a, 2) + pow(b, 2) - 2 * a * b * cos(rad)) << endl;
+    cout << fixed << sin(rad) * b;
 
     return 0;
 }
