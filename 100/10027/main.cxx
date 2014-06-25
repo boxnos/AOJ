148d9c4df7fd7eabbd6b24838dcbe8fabfa64920
boxnos <boxnos@yahoo.com>
2014-06-26 00:20:54 +0900

    accepted in C++11

--- 100/10027/main.cxx
+++ 100/10027/main.cxx
@@ -1,10 +1,37 @@
 #include <iostream>
 using namespace std;
 
+struct Game {
+    int a, b;
+    Game() {
+        a = 0;
+        b = 0;
+    }
+    void score(const string &card_a, const string &card_b) {
+        if (card_a > card_b)
+            a += 3;
+        else if (card_a < card_b)
+            b += 3;
+        else {
+            a += 1;
+            b += 1;
+        }
+    }
+    void board() const {
+        cout << a << " " << b << endl;
+    }
+};
+
 int main()
 {
-    cin;
-    cout << endl;
+    Game game;
+    int n;
+    cin >> n;
+
+    for (string a, b; n-- && cin >> a >> b;)
+        game.score(a, b);
+
+    game.board();
 
     return 0;
 }
