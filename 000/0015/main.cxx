c34e688bd571b9291f313be088cefb35823517ba
boxnos <boxnos@yahoo.com>
2014-06-27 02:44:12 +0900

    accepted in C++11

--- 000/0015/main.cxx
+++ 000/0015/main.cxx
@@ -5,9 +5,40 @@ using namespace std;
 
 class BigInt {
     static const int base = 10;
-    deque<int> digits;
     public:
+    deque<int> digits;
     BigInt() {
+    }
+    BigInt operator +(BigInt x) {
+        BigInt c;
+
+        // add
+        auto di = digits.begin(), xi = x.digits.begin(), end = digits.end();
+        for (; di != end && xi != x.digits.end(); di++, xi++)
+            c.digits.push_back(*di + *xi);
+
+        // set rest
+        if (di == digits.end()) {
+            di = xi;
+            end = x.digits.end();
+        }
+        for (; di != end; di++)
+            c.digits.push_back(*di);
+
+        // nomalize
+        int carry = 0;
+        for (int i = 0; i < c.size(); i++) {
+            c.digits[i] += carry;
+            carry = c.digits[i] / base;
+            c.digits[i] %= base;
+        }
+        if (carry)
+            c.digits.push_back(carry);
+
+        return c;
+    }
+    int size() {
+        return digits.size();
     }
     istream &read(istream &is) {
         digits.clear();
@@ -33,7 +64,13 @@ ostream &operator<<(ostream &os, BigInt &b) { return b.write(os); }
 int main()
 {
     for (int n; cin >> n;)
-        for (BigInt a, b; n-- && cin >> a >> b;)
-            cout << a << " " << b << endl;
+        for (BigInt a, b, c; n-- && cin >> a >> b;) {
+            c = a + b;
+            if (c.size() <= 80)
+                cout << c << endl;
+            else
+                cout << "overflow" << endl;
+        }
+
     return 0;
 }
