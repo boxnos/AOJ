b65e5a93e59dab8ecb000f9c8a3b2d78a5074de9
boxnos <boxnos@yahoo.com>
2014-06-27 04:27:12 +0900

    accepted in C++11

--- 000/0016/main.cxx
+++ 000/0016/main.cxx
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    char comma;
+    double x = 0, y = 0, env = 0;
+    for (double d, t; cin >> d >> comma >> t && (d || t);) {
+        x += sin(env) * d;
+        y += cos(env) * d;
+        env += t * M_PI / 180;
+    }
+    cout << (int) x << endl;
+    cout << (int) y << endl;
 
     return 0;
 }
