c238d7329694afec276274d5d88c14089315c860
boxnos <boxnos@yahoo.com>
2014-06-28 02:06:30 +0900

    fix stream

--- 000/0018/main.cxx
+++ 000/0018/main.cxx
@@ -11,7 +11,7 @@ ostream &operator<<(ostream &os, vector<T> &v) {
     if (v.size()) {
         cout << v.front();
         for (auto vi = v.begin() + 1; vi != v.end(); vi++)
-            cout << " " << *vi;
+            os << " " << *vi;
     }
     return os;
 }
