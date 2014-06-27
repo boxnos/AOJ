caf8ea655d85c2f8ba945ce136272c7872822077
boxnos <boxnos@yahoo.com>
2014-06-28 02:08:21 +0900

    fix stream

--- 000/0018/main.cxx
+++ 000/0018/main.cxx
@@ -9,7 +9,7 @@ using namespace std;
 template <typename T>
 ostream &operator<<(ostream &os, vector<T> &v) {
     if (v.size()) {
-        cout << v.front();
+        os << v.front();
         for (auto vi = v.begin() + 1; vi != v.end(); vi++)
             os << " " << *vi;
     }
