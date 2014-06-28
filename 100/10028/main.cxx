492e8ce04b15a68a4d6cb7f61a8aff1900617880
boxnos <boxnos@yahoo.com>
2014-06-29 01:49:35 +0900

    accepted in C++11

--- 100/10028/main.cxx
+++ 100/10028/main.cxx
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+template <typename T>
+ostream &operator<<(ostream &os, vector<T> &v) {
+    if (v.size()) {
+        os << v.front();
+        for (auto vi = v.begin() + 1; vi != v.end(); vi++)
+            os << " " << *vi;
+    }
+    return os;
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    int n;
+    cin >> n;
+    vector<int> data(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> data[i];
+
+    sort(data.begin(), data.end());
+
+    cout << data << endl;
 
     return 0;
 }
