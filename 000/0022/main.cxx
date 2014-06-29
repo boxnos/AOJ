8a310596a82a7e1aa12b5f6ae56209883587d325
boxnos <boxnos@yahoo.com>
2014-06-30 04:13:28 +0900

    O(n) solution

--- 000/0022/main.cxx
+++ 000/0022/main.cxx
@@ -1,31 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <limits>
+#include <algorithm>
 using namespace std;
-
-int maximum_sub_sequense(vector<int> &s)
-{
-    int max = numeric_limits<int>::min();
-
-    for (auto i = s.begin(); i != s.end(); i++) {
-        int sum = 0;
-        for (auto j = i; j != s.end(); j++) {
-            sum += *j;
-            if (sum > max)
-                max = sum;
-        }
-    }
-
-    return max;
-}
 
 int main()
 {
     for (int n; cin >> n && n;) {
         vector<int> sequence(n);
-        for (int x, i = 0; i < n && cin >> x; i++)
-            sequence[i] = x;
-        cout << maximum_sub_sequense(sequence) << endl;
+        int sum = 0;
+        int max_val = numeric_limits<int>::min();
+        for (int x, i = 0; i < n && cin >> x; i++) {
+            if (sum < 0)
+                sum = x;
+            else
+                sum += x;
+            max_val = max(max_val, sum);
+        }
+        cout << max_val << endl;
     }
 
     return 0;
