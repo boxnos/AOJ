33c6b4109be7fd995732060caa876a1ba92819a2
boxnos <boxnos@yahoo.com>
2014-07-03 01:44:55 +0900

    accepted in C++11

--- 000/0025/main.cxx
+++ 000/0025/main.cxx
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct HitAndBlow
+{
+    vector<int> cards;
+
+    HitAndBlow(){
+        cards = vector<int>(4);
+    }
+
+    void board(const HitAndBlow &other) const {
+        int hit = 0, blow = 0;
+        for (int i = 0; i < cards.size(); i++)
+            for (int j = 0; j < cards.size(); j++)
+                if (i == j) {
+                    if (cards[i] == other.cards[j])
+                        hit++;
+                } else if (cards[i] == other.cards[j])
+                    blow++;
+        cout << hit << " " << blow << endl;
+    }
+};
+
+istream &operator >> (istream &is, HitAndBlow &g)
+{
+    for (int i = 0; i < g.cards.size(); i++)
+        is >> g.cards[i];
+    return is;
+}
 
 int main()
 {
-    cin;
-    cout << endl;
+    for (HitAndBlow braker, maker; cin >> braker >> maker;)
+        maker.board(braker);
 
     return 0;
 }
