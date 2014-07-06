b1b5bd679917e580ca0fde951ae12cd8af567054
boxnos <boxnos@yahoo.com>
2014-07-07 01:38:02 +0900

    accepted in C++11

--- 100/10030/main.cxx
+++ 100/10030/main.cxx
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <set>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    set<int> S, T;
+    int n, q, count = 0;
+
+    cin >> n;
+    for (int x; n-- && cin >> x;)
+        S.insert(x);
+
+    cin >> q;
+    for (int x; q-- && cin >> x;)
+        T.insert(x);
+
+    for (auto x : S)
+        if (T.find(x) != T.end())
+            count++;
+
+    cout << count << endl;
 
     return 0;
 }
