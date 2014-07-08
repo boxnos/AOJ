13dedbea29a49abb89f3dbc61b2eba4da382007d
boxnos <boxnos@yahoo.com>
2014-07-09 01:30:24 +0900

    accepted in C++11

--- 100/10032/main.cxx
+++ 100/10032/main.cxx
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    stack<char> blocks;
+
+    for (string s; cin >> s && s != "quit";) {
+        char c;
+        if (s == "push") {
+            cin >> c;
+            blocks.push(c);
+        } else {
+            cout << blocks.top() << endl;
+            blocks.pop();
+        }
+    }
 
     return 0;
 }
