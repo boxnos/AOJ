d06d52ab31b2611459202e1673668746b6b6c49a
boxnos <boxnos@yahoo.com>
2014-07-09 04:00:35 +0900

    accepted in C++11

--- 100/10033/main.cxx
+++ 100/10033/main.cxx
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    int n;
+    cin >> n;
+
+    vector<stack <char>> blocks(n + 1);
+
+    string s;
+    int mount;
+    for (; cin >> s >> mount && s != "quit";) {
+        if (s == "push") {
+            char c;
+            cin >> c;
+            blocks[mount].push(c);
+        } else if (s == "move") {
+            int to;
+            cin >> to;
+            blocks[to].push(blocks[mount].top());
+            blocks[mount].pop();
+        } else {
+            cout << blocks[mount].top() << endl;
+            blocks[mount].pop();
+        }
+    }
 
     return 0;
 }
