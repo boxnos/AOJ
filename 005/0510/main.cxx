ce364dd368a0b4074ae83bdde47b4e6f0b6a1577
boxnos <boxnos@yahoo.com>
2014-07-14 22:55:23 +0900

    accepted in C++11

--- 005/0510/main.cxx
+++ 005/0510/main.cxx
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    vector<int> sum_of_scores(2, 0);
+
+    for (auto &s : sum_of_scores) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        s = a + b + c + d;
+    }
+
+    cout << max(sum_of_scores[0], sum_of_scores[1]) << endl;
 
     return 0;
 }
