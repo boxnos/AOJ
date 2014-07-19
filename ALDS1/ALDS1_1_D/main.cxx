bb7b4a08c3868b74dd9d2a75b24620cf8bbfbc6c
boxnos <boxnos@yahoo.com>
2014-07-19 23:38:03 +0900

    accepted in C++

--- ALDS1/ALDS1_1_D/main.cxx
+++ ALDS1/ALDS1_1_D/main.cxx
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <limits>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cin;
-    cout << endl;
+    int n;
+    cin >> n;
+
+    int max_profit = numeric_limits<int>::min();
+    int min_value = numeric_limits<int>::max();
+
+    for (int x; n-- && cin >> x;) {
+        max_profit = max(x - min_value, max_profit);
+        min_value = min(x, min_value);
+    }
+
+    cout << max_profit << endl;
 
     return 0;
 }
