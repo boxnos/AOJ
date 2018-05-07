9950264b66cdab82f1707118e8d13ac18b8630bd
boxnos <boxnos@yahoo.com>
2018-05-07 13:14:32 +0900

    First version

---
+++ ITP1/ITP1_3_D/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b, c;
+    int cnt = 0;
+
+    cin >> a >> b >> c;
+
+    for (int i = a; i <= b; i++)
+        if (c % i == 0)
+            cnt++;
+
+    cout << cnt << endl;
+}
