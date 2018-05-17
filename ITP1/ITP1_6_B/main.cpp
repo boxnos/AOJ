b2ad35c0deb94ce857b508ec791917d7232155cc
boxnos <boxnos@yahoo.com>
2018-05-17 23:46:54 +0900

    Use map

--- ITP1/ITP1_6_B/main.cpp
+++ ITP1/ITP1_6_B/main.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
+#include <map>
 #include <vector>
 using namespace std;
 
-enum suits { S, H, C, D };
-
-int main(void)
+int main()
 {
     int n, rank;
     char suit;
-    vector<bool> cards(52, true);
+    vector<char> suits{'S', 'H', 'C', 'D'};
+    map<char, vector<bool>> cards;
+
+    for (int i = 0; i < suits.size(); i++)
+        cards[suits[i]] = vector<bool>(13, true);
 
     cin >> n;
-    while (cin >> suit >> rank && n--) {
-        int s;
-        switch (suit) {
-        case 'S': s = S; break;
-        case 'H': s = H; break;
-        case 'C': s = C; break;
-        case 'D': s = D; break;
-        }
-        cards[s * 13 + rank - 1] = false;
-    }
+    while (cin >> suit >> rank && n--)
+        cards[suit][rank -1 ] = false;
 
-
-    for (int i = 0; i < cards.size(); i++)
-        if (cards[i]) {
-            switch (i / 13) {
-            case S: cout << 'S'; break;
-            case H: cout << 'H'; break;
-            case C: cout << 'C'; break;
-            case D: cout << 'D'; break;
-            }
-            cout  << " " << i % 13 + 1 << endl;
-        }
+    for (int i = 0; i < suits.size(); i++ )
+        for (int j = 0; j < 13; j++)
+            if (cards[suits[i]][j])
+                cout << suits[i] << " " << j + 1 << endl;
 
     return 0;
 }
