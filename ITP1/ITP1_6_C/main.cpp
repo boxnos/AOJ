82bdcc011de1e51987bdcdc4ea76262218e5e657
boxnos <boxnos@yahoo.com>
2018-05-19 00:20:00 +0900

    Fix a bug

--- ITP1/ITP1_6_C/main.cpp
+++ ITP1/ITP1_6_C/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(void)
 {
     int n, b, f, r, v;
-
     vector<vector<vector <int>>> buildings
         = vector<vector <vector <int>>>(4, vector<vector <int>>(3, vector<int>(10, 0)));
 
@@ -14,10 +13,10 @@ int main(void)
     while (cin >> b >> f >> r >> v && n--)
         buildings[b - 1][f - 1][r - 1] += v;
 
-    for (auto x : buildings) {
-        if (x != buildings[0])
+    for (int i = 0; i < buildings.size(); i++) {
+        if (i)
             cout << string(20, '#') << endl;
-        for (auto y: x) {
+        for (auto y: buildings[i]) {
             for (auto z : y)
                 cout << " " << z;
             cout << endl;
