81ba17f6c8aec302c44ebd04ed2784e6db4a07e8
boxnos <boxnos@yahoo.com>
2018-05-22 04:11:19 +0900

    Refactor ITP1_7_A

--- ITP1/ITP1_7_A/main.cpp
+++ ITP1/ITP1_7_A/main.cpp
@@ -2,25 +2,24 @@
 #include <vector>
 using namespace std;
 
-char grade(int m, int f, int r)
-{
-    int mf(m + f);
-    return
-        (m == -1 || f == -1)? 'F':
-        (mf >= 80)? 'A':
-        (mf >= 65)? 'B':
-        (mf >= 50)? 'C':
-        (mf >= 30)?
-            (r >= 50)? 'C' : 'D' :
-        'F';
-}
-
 int main(void)
 {
     int m, f, r;
 
+    auto grade = [&]() {
+        int mf(m + f);
+        return
+            (m == -1 || f == -1)? 'F':
+            (mf >= 80)? 'A':
+            (mf >= 65)? 'B':
+            (mf >= 50)? 'C':
+            (mf >= 30)?
+                (r >= 50)? 'C' : 'D' :
+            'F';
+    };
+
     while (cin >> m >> f >> r && !(m == -1 && f == -1 && r == -1))
-        cout << grade(m, f, r) << endl;
+        cout << grade() << endl;
 
     return 0;
 }
