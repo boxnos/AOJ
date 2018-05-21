ea3085fafb3828951cbabce4bc1c050ff9b58f5c
boxnos <boxnos@yahoo.com>
2018-05-22 03:49:50 +0900

    Fix indent

--- ITP1/ITP1_7_A/main.cpp
+++ ITP1/ITP1_7_A/main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 char grade(int m, int f, int r)
 {
-        int mf(m + f);
-        return
-            (m == -1 || f == -1)? 'F':
-            (mf >= 80)? 'A':
-            (mf >= 65)? 'B':
-            (mf >= 50)? 'C':
-            (mf >= 30)?
-                (r >= 50)? 'C' : 'D' :
-            'F';
+    int mf(m + f);
+    return
+        (m == -1 || f == -1)? 'F':
+        (mf >= 80)? 'A':
+        (mf >= 65)? 'B':
+        (mf >= 50)? 'C':
+        (mf >= 30)?
+            (r >= 50)? 'C' : 'D' :
+        'F';
 }
 
 int main(void)
