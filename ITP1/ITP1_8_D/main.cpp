0b25ec4c5154e47707af953790652d85a0472a6c
boxnos <boxnos@yahoo.com>
2018-05-28 23:28:00 +0900

    Refactor ITP1_8_D

--- ITP1/ITP1_8_D/main.cpp
+++ ITP1/ITP1_8_D/main.cpp
@@ -2,17 +2,6 @@
 #include <string>
 using namespace std;
 
-string search(string s, string p)
-{
-    for (int i = 0, j; i < s.size(); i++) {
-        for (j = 0; j < p.size() && s[(i + j) % s.size()] == p[j]; j++)
-            ;
-        if (j >= p.size())
-            return "Yes";
-    }
-    return "No";
-}
-
 int main()
 {
     string s, p;
@@ -20,7 +9,8 @@ int main()
     getline(cin, s);
     getline(cin, p);
 
-    cout << search(s, p) << endl;
+    s += s;
+    cout << (s.find(p) != -1 ? "Yes" : "No") << endl;
 
     return 0;
 }
