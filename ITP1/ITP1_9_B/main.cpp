3fd1254aa01bc7f6b99bb20b693a4dfd205c8b6b
boxnos <boxnos@yahoo.com>
2018-05-31 02:42:27 +0900

    Refactor ITP1_9_B
    - Use template

--- ITP1/ITP1_9_B/main.cpp
+++ ITP1/ITP1_9_B/main.cpp
@@ -2,13 +2,14 @@
 #include <string>
 using namespace std;
 
+template <typename T> T bind() {T x; cin >> x; return x;}
+
 int main()
 {
 	string s;
 
 	while (cin >> s && s != "-") {
-		int m, h;
-		for (cin >> m; m && cin >> h; m--)
+		for (int m = bind<int>(), h; m && cin >> h; m--)
 			s = s.substr(h) + s.substr(0, h);
 		cout << s << endl;
 	}
