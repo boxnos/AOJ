bcd58c506cbce94a844633d666d2e2bad443406e
boxnos <boxnos@yahoo.com>
2018-06-02 01:25:13 +0900

    Resize ITP1_9_C

--- ITP1/ITP1_9_C/main.cpp
+++ ITP1/ITP1_9_C/main.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main()
 {
-    string t, h;
-    int n, tp = 0, hp = 0;
+	string t, h;
+	int n, tp = 0, hp = 0;
 
-    cin >> n;
+	cin >> n;
 
-    while (n-- && cin >> t >> h) {
-        if (t == h)
-            tp++, hp++;
-        else if (t > h)
-            tp += 3;
-        else
-            hp += 3;
-    }
+	while (n-- && cin >> t >> h)
+		if (t == h)
+			tp++, hp++;
+		else if (t > h)
+			tp += 3;
+		else
+			hp += 3;
 
-    cout << tp << " " << hp << endl;
+	cout << tp << " " << hp << endl;
 
-    return 0;
+	return 0;
 }
