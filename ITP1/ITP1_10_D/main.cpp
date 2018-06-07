518abe3749c87feef0c781ab48eb423efc7ed69d
boxnos <boxnos@yahoo.com>
2018-06-08 00:36:37 +0900

    Use valarray

--- ITP1/ITP1_10_D/main.cpp
+++ ITP1/ITP1_10_D/main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
-#include <cmath>
-#include <cfloat>
+#include <valarray>
 using namespace std;
 
 int main()
 {
     int n;
     cin >> n;
-    vector<double> x(n), y(n), d(4);
+    valarray<double> x(n), y(n), d(4);
 
     for (auto &xi: x) cin >> xi;
     for (auto &yi: y) cin >> yi;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < n; j++)
-            d[i] += pow(abs(x[j] - y[j]), i + 1);
-        d[i] = pow(d[i], 1.0 / (i + 1));
-    }
-
-    d[3] = DBL_MIN;
-    for (int i = 0; i < n; i++)
-        d[3] = max(d[3], abs(x[i] - y[i]));
+    for (int i = 0; i < 3; i++)
+        d[i] = pow(pow(abs(x - y), i + 1).sum(), 1.0 / (i + 1));
+    d[3] = abs(x - y).max();
 
     cout << fixed << setprecision(8);
     for (auto &di: d)
