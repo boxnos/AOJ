71d7945ef2efbf0a2bee0f2b34a30cf49b10dd6d
boxnos <boxnos@yahoo.com>
2018-06-13 05:08:16 +0900

    Refactor ITP1_11_C
    - Use ranged-for
    - Remove space

--- ITP1/ITP1_11_C/main.cpp
+++ ITP1/ITP1_11_C/main.cpp
@@ -5,39 +5,38 @@
 using namespace std;
 
 struct dice {
-    array<int, 6> a;
-    map<int, int> m;
+	array<int, 6> a;
+	map<int, int> m;
 };
 
 int main()
 {
-    dice d[2];
-    array<array<string, 6>, 6> tbl = {
-        "", "2345", "4135", "1425", "3215", "",
-        "3254", "", "0534", "5024", "", "2304",
-        "1453", "5043", "", "", "0513", "4103",
-        "4152", "0542", "", "", "5012", "1402",
-        "2351", "", "5031", "0521", "", "3201",
-        "", "3240", "1430", "4120", "2310", ""};
+	dice d[2];
+	array<array<string, 6>, 6> tbl = {
+		"", "2345", "4135", "1425", "3215", "",
+		"3254", "", "0534", "5024", "", "2304",
+		"1453", "5043", "", "", "0513", "4103",
+		"4152", "0542", "", "", "5012", "1402",
+		"2351", "", "5031", "0521", "", "3201",
+		"", "3240", "1430", "4120", "2310", ""};
 
-    for (auto &x: d) {
-        int i = 0;
-        for (int &y: x.a) {
-            cin >> y;
-            x.m[y] = i++;
-        }
-    }
+	for (auto &x: d) {
+		int i = 0;
+		for (int &y: x.a) {
+			cin >> y;
+			x.m[y] = i++;
+		}
+	}
 
-    bool flag = false;
-    auto s = tbl[ d[0].m[d[1].a[0]] ][ d[0].m[d[1].a[1]] ];
+	bool flag = false;
+	int i = 2;
+	for (auto c: tbl[ d[0].m[d[1].a[0]] ][ d[0].m[d[1].a[1]] ]) {
+		if (d[0].a[c - '0'] != d[1].a[i++]) {
+			flag = false;
+			break;
+		}
+		flag = true;
+	}
 
-    for (size_t i = 0; i < s.size(); i++) {
-        if (d[0].a[s[i] - '0'] != d[1].a[i + 2]) {
-            flag = false;
-            break;
-        }
-        flag = true;
-    }
-
-    cout << (flag ? "Yes" : "No") << endl;
+	cout << (flag ? "Yes" : "No") << endl;
 }
