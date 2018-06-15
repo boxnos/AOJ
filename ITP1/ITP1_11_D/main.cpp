8dc098139a772864c5c730f0392ef778f2a4153a
boxnos <boxnos@yahoo.com>
2018-06-16 02:24:48 +0900

    Accept ITP1_11_D in C++11

--- ITP1/ITP1_11_D/main.cpp
+++ ITP1/ITP1_11_D/main.cpp
@@ -1,50 +1,39 @@
 #include <iostream>
 #include <array>
-#include <vector>
 #include <map>
-#include <string>
 using namespace std;
 
-typedef array<int, 6> dice;
-struct DB {
-    map<int, DB> d;
-};
-
-bool append(dice &d, int i, DB &db)
-{
-    int f = d[i];
-    if (!db.d.count(f)) {
-        db.d[f] = *(new DB);
-        if (i == 5)
-            return false;
-    } else if (i == 5)
-        return true;
-    return append(d, i + 1, db.d[f]);
-}
+typedef unsigned long ul;
+typedef array<ul, 6> dice;
 
 bool f(int n)
 {
     static const array<dice, 6> tbl = {
             0,1,2,3,4,5, 1,5,2,3,0,4, 2,1,5,0,4,3,
             3,1,0,5,4,2, 4,0,2,3,5,1, 5,1,3,2,4,0};
-    DB db;
-    dice d, tmp;
+    dice d;
+    map<ul, bool> db;
+    array<ul, 24> r;
 
     while (n--) {
-        for (int &a: d)
+        for (ul &a: d)
             cin >> a;
-        for (dice t: tbl)
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 6; j++)
-                    tmp[j] = d[t[j]];
-                if (append(tmp , 0, db))
+        int i = 0;
+        for (dice t: tbl) {
+            for (int j = 0; j < 4; j++) {
+                ul u = 0;
+                for (int k = 0; k < 6; k++)
+                    u += (d[t[k]] + 1) << k * 10;
+                if (db.count(u))
                     return true;
-                if (i != 3) {
-                    int x = t[1]; t[1] = t[2]; t[2] = t[4]; t[4] = t[3]; t[3] = x;
-                }
+                r[i * 4 + j] = u;
+                ul x = t[1]; t[1] = t[2]; t[2] = t[4]; t[4] = t[3]; t[3] = x;
             }
+            i++;
+        }
+        for (ul x : r)
+            db[x] = true;
     }
-
     return false;
 }
 
@@ -52,6 +41,5 @@ int main()
 {
     int n;
     cin >> n;
-
     cout << (f(n) ? "No" : "Yes") << endl;
 }
