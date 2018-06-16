a78178e64f86b45a0a795e153d312a79b1eac5a5
boxnos <boxnos@yahoo.com>
2018-06-16 22:42:26 +0900

    Refactor ITP1_11_D
    - Use unordered_map
    - Remove space

--- ITP1/ITP1_11_D/main.cpp
+++ ITP1/ITP1_11_D/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <array>
-#include <map>
+#include <unordered_map>
 using namespace std;
 
 typedef unsigned long ul;
@@ -8,38 +8,40 @@ typedef array<ul, 6> dice;
 
 bool f(int n)
 {
-    static const array<dice, 6> tbl = {
-            0,1,2,3,4,5, 1,5,2,3,0,4, 2,1,5,0,4,3,
-            3,1,0,5,4,2, 4,0,2,3,5,1, 5,1,3,2,4,0};
-    dice d;
-    map<ul, bool> db;
-    array<ul, 24> r;
+	static const array<dice, 6> tbl = {
+			0,1,2,3,4,5, 1,5,2,3,0,4, 2,1,5,0,4,3,
+			3,1,0,5,4,2, 4,0,2,3,5,1, 5,1,3,2,4,0};
+	dice d;
+	unordered_map<ul, bool> db;
+	array<ul, 24> r;
 
-    while (n--) {
-        for (ul &a: d)
-            cin >> a;
-        int i = 0;
-        for (dice t: tbl) {
-            for (int j = 0; j < 4; j++) {
-                ul u = 0;
-                for (int k = 0; k < 6; k++)
-                    u += (d[t[k]] + 1) << k * 10;
-                if (db.count(u))
-                    return true;
-                r[i * 4 + j] = u;
-                ul x = t[1]; t[1] = t[2]; t[2] = t[4]; t[4] = t[3]; t[3] = x;
-            }
-            i++;
-        }
-        for (ul x : r)
-            db[x] = true;
-    }
-    return false;
+	while (n--) {
+		for (ul &x: d)
+			cin >> x;
+		int i = 0;
+		for (dice t: tbl) {
+			for (int j = 0; j < 4; j++) {
+				ul l = 0;
+				for (int k = 0; k < 6; k++)
+					l += (d[t[k]] + 1) << k * 10;
+				if (db.count(l))
+					return true;
+				r[i * 4 + j] = l;
+				ul x = t[1]; t[1] = t[2]; t[2] = t[4]; t[4] = t[3]; t[3] = x;
+			}
+			i++;
+		}
+		for (ul l : r)
+			db[l] = true;
+	}
+	return false;
 }
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << (f(n) ? "No" : "Yes") << endl;
+	int n;
+	cin >> n;
+	cout << (f(n) ? "No" : "Yes") << endl;
 }
+
+/* vim: set ts=4 noet: */
