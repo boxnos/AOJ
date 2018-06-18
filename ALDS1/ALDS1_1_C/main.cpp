6542105468023e9e13edd36841a82a2d4fc79142
boxnos <boxnos@yahoo.com>
2018-06-19 04:21:02 +0900

    Chainge algorithm to trial division

--- ALDS1/ALDS1_1_C/main.cpp
+++ ALDS1/ALDS1_1_C/main.cpp
@@ -1,36 +1,31 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
-#include <algorithm>
 using namespace std;
+
+bool is_prime(int x)
+{
+    if (x == 2)
+        return true;
+    else if (x % 2 == 0)
+        return false;
+
+    for (int i = 3; i <= sqrt(x) ; i += 2)
+        if (x % i == 0)
+            return false;
+
+    return true;
+}
 
 int main()
 {
-    int n, c = 0, m = 0, counter = 0;
+    int n, x, c = 0;
+
     cin >> n;
-    vector<int> d(n);
-    for (int &x : d) {
+    while (n--) {
         cin >> x;
-        m = max(m, x);
-    }
-
-    vector<bool> p(m / 2 + 1, true);
-    for (int i = 3, e = sqrt(m); i <= e; i += 2) {
-        if (!p[i / 2])
-            continue;
-        for (int j = i * i / 2; j <= m / 2; j += i) {
-            p[j] = false;
-            counter++;
-        }
-    }
-
-    for (int x: d)
-        if (x == 2 || (x % 2 && p[x / 2]))
+        if (is_prime(x))
             c++;
+    }
 
     cout << c << endl;
-
-    cerr << counter << endl;
-
-    return 0;
 }
