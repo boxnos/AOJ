85da334dc6fe70b047294ed39462a14bebe0aa20
boxnos <boxnos@yahoo.com>
2018-06-19 23:47:04 +0900

    Optimize ALDS1_1_B
    - Use Trial Division and Sieve of Eratosthenes

--- ALDS1/ALDS1_1_C/main.cpp
+++ ALDS1/ALDS1_1_C/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 using namespace std;
+
+vector<int> sieve;
+
+void make_sieve(int m) {
+    const int l = m / 2 + 1;
+    vector<bool> s(l, true);
+    for (int i = 3, e = sqrt(m); i <= e; i += 2) {
+        if (!s[i / 2])
+            continue;
+        for (int j = i * i / 2; j < l; j += i)
+            s[j] = false;
+    }
+    for (int i = 1; i < l; i++)
+        if (s[i])
+            sieve.push_back(i * 2 + 1);
+}
 
 bool is_prime(int x)
 {
@@ -9,8 +26,11 @@ bool is_prime(int x)
     else if (x % 2 == 0)
         return false;
 
-    for (int i = 3; i <= sqrt(x) ; i += 2)
-        if (x % i == 0)
+    const int e = sqrt(x);
+    for (int p : sieve)
+        if (p > e)
+            return true;
+        else if (x % p == 0)
             return false;
 
     return true;
@@ -18,14 +38,19 @@ bool is_prime(int x)
 
 int main()
 {
-    int n, x, c = 0;
-
+    int n, c = 0, m = 0;
     cin >> n;
-    while (n--) {
+    vector<int> d(n);
+    for (int &x: d) {
         cin >> x;
+        m = max(m, x);
+    }
+
+    make_sieve(sqrt(m));
+
+    for (int x: d)
         if (is_prime(x))
             c++;
-    }
 
     cout << c << endl;
 }
