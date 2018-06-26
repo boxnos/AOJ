7aa7452665514f6fb480f92ed1edae60cdb94938
boxnos <boxnos@yahoo.com>
2018-06-26 23:08:52 +0900

    Accept ALDS1_2_C in C++11

---
+++ ALDS1/ALDS1_2_C/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+typedef string s;
+typedef vector<s> vs;
+
+void p(vs &v)
+{
+	for (s &x: v)
+		cout << (&x == &v[0] ? "" : " ") << x;
+	cout << endl;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	vs bs(n), ss(n);
+	for (s &x: bs)
+		cin >> x;
+	ss = bs;
+
+	auto cmp = [](s a, s b){return a[1] < b[1];};
+	stable_sort(bs.begin(), bs.end(), cmp);
+	p(bs);
+	cout << "Stable" << endl;
+
+	for (auto i = ss.begin(), e = ss.end(); i != e - 1; i++) {
+		auto min = i, j = i;
+		for (; j != e; j++)
+			if (cmp(*j, *min))
+				min = j;
+		if (i != min)
+			swap(*i, *min);
+	}
+	p(ss);
+	cout << (bs == ss ? "Stable" : "Not stable") << endl;
+}
+
+/* vim: set ts=4 noet: */
