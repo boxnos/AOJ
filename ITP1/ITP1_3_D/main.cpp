5a70af96f269a691bbef809afd1405f68e935397
boxnos <boxnos@yahoo.com>
2018-06-30 04:32:33 +0900

    Refactor ITP1_3_D

--- ITP1/ITP1_3_D/main.cpp
+++ ITP1/ITP1_3_D/main.cpp
@@ -1,41 +1,15 @@
 #include <iostream>
-#include <cmath>
-#include <map>
-#include <vector>
-#include <iterator>
 using namespace std;
 
-vector<int> divisors;
-void make_divisors(map<int, int>::iterator begin, map<int, int>::iterator end, int product) {
-    if (begin == end)
-        divisors.push_back(product);
-    else
-        for (int i = 0; i <= begin->second; i++)
-            make_divisors(std::next(begin), end, product * pow(begin->first, i));
-}
-
-int main(void)
+int main()
 {
-    int a, b, c;
-    int cnt = 0;
-    map<int, int> factors;
+	int a, b, c, n = 0;
 
-    cin >> a >> b >> c;
+	cin >> a >> b >> c;
 
-    for (int i = 2, e = c; e != 1 || i <= sqrt(c); i++)
-        while (e % i == 0) {
-            e /= i;
-            if (factors.count(i))
-                factors[i]++;
-            else
-                factors.insert(pair<int, int>(i, 1));
-        }
+	for (; a <= b; a++)
+		if (c % a == 0)
+			n++;
 
-    make_divisors(factors.begin(), factors.end(), 1);
-
-    for (int i = 0; i < divisors.size(); i++)
-        if (a <= divisors[i] && divisors[i] <= b)
-            cnt++;
-
-    cout << cnt << endl;
+	cout << n << endl;
 }
