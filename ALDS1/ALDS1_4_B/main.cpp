55f81870bf40da2137ba68b0434a00c292217394
boxnos <boxnos@yahoo.com>
2018-07-07 08:33:03 +0900

    Refactor in()

--- ALDS1/ALDS1_4_B/main.cpp
+++ ALDS1/ALDS1_4_B/main.cpp
@@ -4,12 +4,11 @@ using namespace std;
 
 static inline int in()
 {
-	int n = 0, c = getchar_unlocked();
-	do {
-		n = 10 * n + c - '0';
-		c = getchar_unlocked();
-	} while (c >= '0');
-	return n;
+    int c, n = 0;
+    while ((c = getchar_unlocked()) >= '0' && c <= '9')
+		// n = 10 * n + (c - '0');
+		n = (n << 3) + ( n << 1 ) + c - '0';
+    return n;
 }
 
 int main()
