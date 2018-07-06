e3fd4be032cbd3cb7f87c1c91646690a86f4e185
boxnos <boxnos@yahoo.com>
2018-07-07 01:53:14 +0900

    Accept in C++

--- ALDS1/ALDS1_4_A/main.cpp
+++ ALDS1/ALDS1_4_A/main.cpp
@@ -3,25 +3,25 @@
 #include <algorithm>
 using namespace std;
 
+#define V(a) a.begin(), a.end()
+typedef vector<int> vi;
+
 int main()
 {
-	int n, q, C = 0;
+	vi a[2], C;
 
-	cin >> n;
-	vector<int> S(n);
-	for (int &x: S)
-		cin >> x;
-
-	cin >> q;
-	while (q--) {
-		int x;
-		cin >> x;
-		auto res = find(S.begin(), S.end(), x);
-		if (res != S.end())
-			C++;
+	for (auto &i : a) {
+		int n;
+		cin >> n;
+		i = vi(n);
+		for (int &j : i)
+			cin >> j;
+		sort(V(i));
 	}
 
-	cout << C << endl;
+	set_intersection(V(a[0]), V(a[1]), inserter(C, C.end()));
+
+	cout << C.size() << endl;
 }
 
 /* vim: set ts=4 noet: */
