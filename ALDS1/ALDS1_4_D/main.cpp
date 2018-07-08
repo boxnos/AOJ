17540ad3d8dd2d13bb715e290650dcbc7d6b6e54
boxnos <boxnos@yahoo.com>
2018-07-09 02:19:30 +0900

    Optimize ALDS1_4_D

--- ALDS1/ALDS1_4_D/main.cpp
+++ ALDS1/ALDS1_4_D/main.cpp
@@ -4,44 +4,37 @@ using namespace std;
 
 int main()
 {
-	int n, K, min = 0, max = 0, P;
+	int n, K, l = 0, r = 0, P;
 	scanf("%d %d", &n, &K);
 	vector<int> w(n);
 	for (int &x: w) {
 		scanf("%d", &x);
-		min = min > x ? min : x;
-		max += x;
+		l = l > x ? l : x;
+		r += x;
 	}
-	min--;
-	
-	for (;;) {
-		P = (max + min) / 2;
-		auto i = w.begin(), e = w.end(), last = e - 1;
+	P = (r - l) / n + l;
+	while (l != r) {
+		auto i = w.begin(), e = w.end(), ee = e - 1;
 		int k = 0, p = 0;
 		for (; i != e && k < K; i++) {
-			int tmp = p + *i;
-			if (tmp > P) {
+			int t = p + *i;
+			if (t < P)
+				p = t;
+			else if (t > P)
 				p = *i, k++;
-				if (i == last)
-					k++;
-			} else if (tmp == P)
-				p = 0, k++;
 			else {
-				if (i == last) k++;
-				p = tmp;
+				p = 0;
+				if (i != ee)
+					k++;
 			}
 		}
-		if (i == e && k <= K)
-			max = P;
+		if (i == e && k < K)
+			r = P;
 		else
-			min = P;
-
-		if (max == min + 1) {
-			P = max;
-			break;
-		}
+			l = P + 1;
+		P = (l + r) / 2;
 	}
-	printf("%d\n", P);
+	printf("%d\n", r);
 }
 
 /* vim: set ts=4 noet: */
