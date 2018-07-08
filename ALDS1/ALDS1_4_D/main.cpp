bad38074fdbd580bc73dc571df64291c7b7853c6
boxnos <boxnos@yahoo.com>
2018-07-08 23:34:25 +0900

    Optimize ALDS1_4_D

--- ALDS1/ALDS1_4_D/main.cpp
+++ ALDS1/ALDS1_4_D/main.cpp
@@ -6,36 +6,36 @@ int main()
 {
 	int n, K, min = 0, max = 0, P;
 	scanf("%d %d", &n, &K);
-
 	vector<int> w(n);
 	for (int &x: w) {
 		scanf("%d", &x);
 		min = min > x ? min : x;
 		max += x;
 	}
-	min -= 1;
+	min--;
 	
 	for (;;) {
 		P = (max + min) / 2;
-		auto i = w.begin(), e = w.end(), j = i;
-		int k = 0;
-		for (; i != e && j != e && k < K; i++, k++) {
-			int p = 0;
-			for (j = i; j != e; j++) {
-				if (p + *j > P) {
-					i = j - 1;
-					break;
-				} else if (p + *j == P) {
-					i = j;
-					break;
-				} else
-					p += *j;
+		auto i = w.begin(), e = w.end(), last = e - 1;
+		int k = 0, p = 0;
+		for (; i != e && k < K; i++) {
+			int tmp = p + *i;
+			if (tmp > P) {
+				p = *i, k++;
+				if (i == last)
+					k++;
+			} else if (tmp == P)
+				p = 0, k++;
+			else {
+				if (i == last) k++;
+				p = tmp;
 			}
 		}
-		if ((i == e || j == e) && k <= K)
+		if (i == e && k <= K)
 			max = P;
 		else
 			min = P;
+
 		if (max == min + 1) {
 			P = max;
 			break;
