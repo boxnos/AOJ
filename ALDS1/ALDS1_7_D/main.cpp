4b31faf5ce5a72a80037713caea6e74e983bc5a4
boxnos <boxnos@yahoo.com>
2018-08-06 03:19:57 +0900

    Refactor output library

--- ALDS1/ALDS1_7_D/main.cpp
+++ ALDS1/ALDS1_7_D/main.cpp
@@ -13,7 +13,8 @@ si int in() {
 	while ((c = gcu()) >= '0' && c <= '9') n = 10 * n + (c - '0');
 	return minus ? -n : n; }
 #define pcu putchar_unlocked
-si void out(int n) {
+#define svo si void out
+svo(int n) {
 	static char buf[20];
 	char *p = buf;
 	bool minus = false;
@@ -22,16 +23,12 @@ si void out(int n) {
 	else while (n) *p++ = n % 10 + '0', n /= 10;
 	if (minus) *p++ = '-';
 	while (p != buf) pcu(*--p); }
-si void out(const char *s){while(*s)pcu(*s++);}
-si void out(char c){pcu(c);}
+svo(const char *s){while(*s)pcu(*s++);}
+svo(char c){pcu(c);}
 template <typename head, typename... tail>
-si void out(head&& h, tail&&... t){out(h);out(move(t)...);}
+svo(head&& h, tail&&... t){out(h);out(move(t)...);}
 template <typename T>
-si void out(vector<T> v) {
-	for (int &x: v)
-		out(&x == &v[0] ? "": " ", x);
-	out('\n');
-}
+svo(vector<T> v){for(int &x:v)out(&x == &v[0]?"":" ",x);out('\n');}
 
 typedef vector<int> v;
 typedef v::iterator vi;
