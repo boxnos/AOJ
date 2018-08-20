f782c598eee466bd8413280a8b001090fdaf08e9
boxnos <boxnos@yahoo.com>
2018-08-20 18:03:44 +0900

    Optimize ALDS1_11_D
    Use Union-Find algorithm

--- ALDS1/ALDS1_11_D/main.cpp
+++ ALDS1/ALDS1_11_D/main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 #define gcu getchar_unlocked
 si int in() {
 	int c, n = 0;
-	bool minus = false;
-	if ((c = gcu()) == '-') minus = true; else ungetc(c, stdin);
+	//bool minus = false;
+	//if ((c = gcu()) == '-') minus = true; else ungetc(c, stdin);
 	while ((c = gcu()) >= '0' && c <= '9') n = 10 * n + (c - '0');
-	return minus ? -n : n; }
+	return n; }
+	//return minus ? -n : n; }
 si void scan(char *s) {while (!isspace(*s++ = gcu()));}
 #define pcu putchar_unlocked
 #define svo si void out
@@ -18,7 +19,7 @@ template <typename T>
 svo(T n) {
 	static char buf[20];
 	char *p = buf;
-	if(n < 0) pcu('-'), n *= -1;
+	//if(n < 0) pcu('-'), n *= -1;
 	if (!n) *p++ = '0';
 	else while (n) *p++ = n % 10 + '0', n /= 10;
 	while (p != buf) pcu(*--p); }
@@ -31,50 +32,52 @@ svo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
 template <typename head, typename... tail>
 svo(head&& h, tail&&... t){out(h);out(move(t)...);}
 
-typedef vector<int> v;
-struct graph {
-	struct node {v n; int gid = 0;};
+struct union_find {
+	struct node {int p, r = 0;};
 	vector<node> nodes;
-	int gid_counter = 1;
 
-	graph(int n) {
+	union_find(int n) {
 		nodes.resize(n);
-	}
-	void read(int n) {
-		while (n--) {
-			int s = in(), t = in();
-			nodes[s].n.push_back(t);
-			nodes[t].n.push_back(s);
-		}
-	}
-	void traverse(int id, int gid) {
-		node &n = nodes[id];
-		if (n.gid)
-			return;
-		n.gid = gid ? gid : gid_counter++;
-		for (int i: n.n)
-			traverse(i, n.gid);
-	}
-	void traverse() {
 		int i = 0;
 		for (node &n: nodes)
-			traverse(i++, n.gid);
+			n.p = i++;
 	}
-	bool find(int s, int t) {
-		return nodes[s].gid == nodes[t].gid;
+	int find(int x) {
+		int &p = nodes[x].p;
+		if (p == x)
+			return x;
+		else {
+			p = find(p);
+			return p;
+		}
+	}
+	void _union(int a, int b) {
+		int ar = find(a), br = find(b);
+		node &an = nodes[ar], &bn = nodes[br];
+		if (an.r > bn.r)
+			bn.p = ar;
+		else if (an.r > bn.r)
+			an.p = br;
+		else if (ar != br) {
+			bn.p = ar;
+			an.r++;
+		}
+	}
+	void read(int n) {
+		int a;
+		while(n--) {
+			a = in();
+			_union(a, in());
+		}
 	}
 };
 
 int main() {
-	int n = in(), m = in();
-	graph g(n);
-	g.read(m);
-	g.traverse();
+	union_find u(in());
+	u.read(in());
 	int q = in();
-	while (q--) {
-		int s = in();
-		out(g.find(s, in())? "yes" : "no", '\n');
-	}
+	while (q--)
+		out(u.find(in()) == u.find(in()) ? "yes" : "no", '\n');
 }
 
 /* vim: set ts=4 noet: */
