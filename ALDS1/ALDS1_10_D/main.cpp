b3cd9c4653011757a4e3247539a27dea5db575e2
boxnos <boxnos@yahoo.com>
2018-08-26 17:33:58 +0900

    Clean code

--- ALDS1/ALDS1_10_D/main.cpp
+++ ALDS1/ALDS1_10_D/main.cpp
@@ -1,39 +1,7 @@
 #include <cstdio>
-#include <cctype>
 #include <cfloat>
 #include <vector>
 using namespace std;
-
-#define si static inline
-#define gcu getchar_unlocked
-si int in() {
-	int c, n = 0;
-	bool minus = false;
-	if ((c = gcu()) == '-') minus = true; else ungetc(c, stdin);
-	while ((c = gcu()) >= '0' && c <= '9') n = 10 * n + (c - '0');
-	//return n; }
-	return minus ? -n : n; }
-si void scan(char *s) {while (!isspace(*s++ = gcu()));}
-#define pcu putchar_unlocked
-#define svo si void out
-template <typename T>
-svo(T n) {
-	static char buf[20];
-	char *p = buf;
-	//if(n < 0) pcu('-'), n *= -1;
-	if (!n) *p++ = '0';
-	else while (n) *p++ = n % 10 + '0', n /= 10;
-	while (p != buf) pcu(*--p); }
-svo(const char *s){while(*s)pcu(*s++);}
-svo(char *s){while(*s)pcu(*s++);}
-svo(char c){pcu(c);}
-template <typename T>
-//svo(vector<T> &v){for(T &x:v)out(' '),out(x);out('\n');}
-svo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
-template <typename head, typename... tail>
-svo(head&& h, tail&&... t){out(h);out(move(t)...);}
-#undef svo
-#undef si
 
 #define v vector
 
@@ -71,7 +39,8 @@ struct OBST {
 };
 
 int main() {
-	int n = in();
+	int n;
+	scanf("%d", &n);
 	OBST obst;
 	obst.read(n);
 	printf("%.8lf\n", obst.cost(n));
