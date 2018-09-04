7016eb3860d0fece8b7030f074173c03bd0341ea
boxnos <boxnos@yahoo.com>
2018-09-04 20:00:55 +0900

    A* version for ALDS1_6_D

--- ALDS1/ALDS1_6_D/main.cpp
+++ ALDS1/ALDS1_6_D/main.cpp
@@ -2,63 +2,105 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
+#include <set>
 using namespace std;
 
-typedef vector<int> v;
-
+#define si static inline
 #define gcu getchar_unlocked
-static inline int in()
-{
-	int c, n = 0;
-	while ((c = gcu()) >= '0' && c <= '9') n = 10 * n + (c - '0');
-	return n;
-}
+si int in() {
+	int n = 0, c = gcu();
+	// bool minus = false; if (c == '-') minus = true, c = gcu();
+	do {n = 10 * n + (c - '0'), c = gcu();} while (c >= '0');
+	return n; }
+	// return minus ? -n : n; }
+si void scan(char *s) {while (!isspace(*s++ = gcu()));}
+#define pcu putchar_unlocked
+#define svo si void out
+template <typename T>
+svo(T n) {
+	static char buf[20];
+	char *p = buf;
+	//if(n < 0) pcu('-'), n *= -1;
+	if (!n) *p++ = '0';
+	else while (n) *p++ = n % 10 + '0', n /= 10;
+	while (p != buf) pcu(*--p); }
+svo(const char *s){while(*s)pcu(*s++);}
+svo(char *s){while(*s)pcu(*s++);}
+svo(char c){pcu(c);}
+template <typename T>
+//svo(vector<T> &v){for(T &x:v)out(' '),out(x);out('\n');}
+svo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+template <typename head, typename... tail>
+svo(head&& h, tail&&... t){out(h);out(move(t)...);}
+#undef svo
+#undef si
 
-int hn(v &w, v &s) {
-	int c = 0;
-	auto b = s.begin();
-	for (int a: w)
-		if (a != *b++)
-			c += a;
-	return c;
-}
 
-int mcs(v w, v &s, int g, int h, int limit)
-{
-	if (g + h > limit)
-		return -1;
-	if (!h)
-		return g;
+struct mcs {
+	typedef vector<int> v;
+	struct node {
+		v w;
+		int f, g, h;
+	};
+	node n;
+	v s;
 
-	for (size_t i = 0; i < w.size() - 1; i++) {
-		for (size_t j = i + 1; j < w.size(); j++) {
-	//		if (w[i] == s[j] || w[j] == s[i]) {
-				swap(w[i], w[j]);
-				int res = mcs(w, s, g + w[i] + w[j],
-							  h - w[i] - w[j] + (w[i] == s[i] ? 0 : w[i]) + + (w[j] == s[j] ? 0 : w[j]), limit);
-				if (res != -1)
-					return res;
-				swap(w[i], w[j]);
-	//		}
-		}
+	mcs(int i) {
+		n.w.resize(i);
+		s.resize(i);
+
+		for (int &x : n.w)
+			x = in();
+		s = n.w;
+		sort(s.begin(), s.end());
+
+		n.h = 0;
+		auto b = s.begin();
+		for (int a: n.w)
+			if (a != *b++)
+				n.h += a;
+		n.f = n.h;
+		n.g = 0;
 	}
-	return -1;
+
+	int solve() {
+		priority_queue<node> q;
+		set<v> m;
+		q.push(n);
+		while (!q.empty()) {
+			node a = q.top();
+			q.pop();
+
+			if (!a.h)
+				return a.f;
+			if (m.count(a.w))
+				continue;
+			m.insert(a.w);
+			for (size_t i = 0; i < s.size() - 1; i++) {
+				for (size_t j = i + 1; j < s.size(); j++) {
+					node b = a;
+					auto &w = b.w;
+					swap(w[i], w[j]);
+					b.g += w[i] + w[j];
+					b.h += - w[i] - w[j] + (w[i] == s[i] ? 0 : w[i]) + (w[j] == s[j] ? 0 : w[j]);
+					b.f = b.g + b.h;
+					q.push(b);
+				}
+			}
+		}
+		return -1;
+	}
+};
+
+bool operator < (const mcs::node &a, const mcs::node &b) {
+	return a.f > b.f;
 }
 
 int main()
 {
-	v w(in());
-
-	for (int &x : w)
-		x = in();
-	v s = w;
-
-	sort(s.begin(), s.end());
-	//printf("%d\n", hn(w, s));
-	int h = hn(w, s), r = -1;
-	for (int limit = h; r == -1; limit += 1)
-		r = mcs(w, s, 0, h, limit);
-	printf("%d\n", r);
+	mcs m(in());
+	printf("%d\n", m.solve());
 }
 
 /* vim: set ts=4 noet: */
