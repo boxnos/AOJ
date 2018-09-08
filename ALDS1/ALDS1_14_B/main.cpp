c80f33aac9fd4b624a76454909ada8d8832e45e5
boxnos <boxnos@yahoo.com>
2018-09-09 02:04:59 +0900

    Naive finite automaton version for ALDS1_14_B
    See Introduction Algorithms 32.2

--- ALDS1/ALDS1_14_B/main.cpp
+++ ALDS1/ALDS1_14_B/main.cpp
@@ -1,35 +1,73 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-void rk(string &T, string &P, int d, int q) {
-	int n = T.size(), m = P.size();
-	int p = 0, t = 0, h = 1;
+typedef unordered_map<char, int> sigma;
+typedef vector<vector<int>> delta;
 
-	for (int i = 0; i < m - 1; i++)
-		h = h * d % q;
+struct finite_automaton {
+	sigma s;
+	delta d;
+	string T, P;
 
-	for (int i = 0; i < m; i++) {
-		p = (d * p + P[i]) % q;
-		t = (d * t + T[i]) % q;
+	finite_automaton() {
+		cin >> T >> P;
+		int i = 0;
+		for (char c: P)
+			if (!s.count(c))
+				s[c] = i++;
+		d = delta(P.size() + 1, vector<int>(s.size()));
+		transition();
 	}
 
-	for (int s = 0; s <= n - m; s++) {
-		if (p == t && P == T.substr(s, m))
-			cout << s << endl;
-		if (s < n - m)
-			t =  ((t - T[s] * h) * d + T[s + m]) % q;
-		if (t < 0)
-            t = (t + q);
+	bool cmp(int k, int q, char a) {
+		string pq = P.substr(0, q) + a;
+		return P.substr(0, k) == pq.substr(pq.size() - k, k);
 	}
-}
+
+	void transition() {
+		for (size_t q = 0; q < d.size(); q++)
+			for (auto a: s) {
+				int k = min(d.size(), q + 2);
+				do {
+					k--;
+				} while (!cmp(k, q, a.first));
+				d[q][a.second] = k;
+			}
+	}
+
+	void matcher() {
+		size_t q = 0;
+		for (size_t i = 0; i < T.size(); i++) {
+			if (s.count(T[i]))
+				q = d[q][s[T[i]]];
+			else
+				q = 0;
+			if (q == P.size())
+				cout << i - P.size() + 1 << endl;
+		}
+	}
+
+	void print() {
+		size_t k = 0;
+		for (auto i: d) {
+			cerr << (k < P.size() ? P[k++] : ' ');
+			for (int j: i)
+				cerr << " " << j;
+			cerr << endl;
+		}
+	}
+};
 
 int main() {
-	string t, p;
-	cin >> t >> p;
+	cin.tie(0);
+    ios::sync_with_stdio(false);
+	finite_automaton fa = finite_automaton();
 
-	if (t.size() >= p.size())
-		rk(t, p, 256, 26339);
+	fa.matcher();
+	//fa.print();
 }
 
 /* vim: set ts=4 noet: */
