54863a66a11c2bbbc6ee46e580313678b3f8624b
boxnos <boxnos@yahoo.com>
2018-09-10 01:33:12 +0900

    Accept ALDS1_14_B in C++11
    - Use KMP algorithm
    - See CLRS 32.4

--- ALDS1/ALDS1_14_B/main.cpp
+++ ALDS1/ALDS1_14_B/main.cpp
@@ -1,73 +1,63 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
-typedef unordered_map<char, int> sigma;
-typedef vector<vector<int>> delta;
-
-struct finite_automaton {
-	sigma s;
-	delta d;
+struct KMP {
+	vector<int> pi;
 	string T, P;
 
-	finite_automaton() {
+	KMP() {
 		cin >> T >> P;
-		int i = 0;
-		for (char c: P)
-			if (!s.count(c))
-				s[c] = i++;
-		d = delta(P.size() + 1, vector<int>(s.size()));
-		transition();
+		prifix_function();
 	}
 
-	bool cmp(int k, int q, char a) {
-		string pq = P.substr(0, q) + a;
-		return P.substr(0, k) == pq.substr(pq.size() - k, k);
-	}
-
-	void transition() {
-		for (size_t q = 0; q < d.size(); q++)
-			for (auto a: s) {
-				int k = min(d.size(), q + 2);
-				do {
-					k--;
-				} while (!cmp(k, q, a.first));
-				d[q][a.second] = k;
-			}
+	void prifix_function() {
+		pi.resize(P.size());
+		int k = -1;
+		pi[0] = k;
+		for (size_t q = 1; q < P.size(); q++) {
+			while (k >= 0 && P[k + 1] != P[q])
+				k = pi[k];
+			if (P[k + 1] == P[q])
+				k++;
+			pi[q] = k;
+		}
 	}
 
 	void matcher() {
-		size_t q = 0;
+		int q = -1;
+		int m = P.size();
 		for (size_t i = 0; i < T.size(); i++) {
-			if (s.count(T[i]))
-				q = d[q][s[T[i]]];
-			else
-				q = 0;
-			if (q == P.size())
-				cout << i - P.size() + 1 << endl;
+			while (q >= 0 && P[q + 1] != T[i])
+				q = pi[q];
+			if (P[q + 1] == T[i])
+				q++;
+			if (q == m - 1) {
+				cout << i - m + 1 << endl;
+				q = pi[q];
+			}
 		}
 	}
-
 	void print() {
-		size_t k = 0;
-		for (auto i: d) {
-			cerr << (k < P.size() ? P[k++] : ' ');
-			for (int j: i)
-				cerr << " " << j;
-			cerr << endl;
-		}
+		for (char c: P)
+			cerr << "  " << c << " ";
+		cerr << endl;
+		for (int i: pi)
+			cerr << setw(3) << i << " ";
+		cerr << endl;
 	}
 };
 
 int main() {
 	cin.tie(0);
     ios::sync_with_stdio(false);
-	finite_automaton fa = finite_automaton();
+	KMP kmp = KMP();
 
-	fa.matcher();
-	//fa.print();
+	kmp.matcher();
+	//kmp.print();
 }
 
 /* vim: set ts=4 noet: */
