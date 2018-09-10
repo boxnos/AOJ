d060665715dfa79be67d2471dc4ae613ec029aa5
boxnos <boxnos@yahoo.com>
2018-09-11 00:06:33 +0900

    Optimize I/O

--- ALDS1/ALDS1_14_B/main.cpp
+++ ALDS1/ALDS1_14_B/main.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
+#include <cstdio>
+#include <cctype>
 #include <iomanip>
-#include <string>
 #include <vector>
-#include <unordered_map>
 using namespace std;
+
+#define si static inline
+#define gcu getchar_unlocked
+si int in() {
+	int n = 0, c = gcu();
+	// bool minus = false; if (c == '-') minus = true, c = gcu();
+	do {n = 10 * n + (c - '0'), c = gcu();} while (c >= '0');
+	return n; }
+	// return minus ? -n : n; }
+si void scan(char *s) {while (!isspace(*s++ = gcu())); *s = '\0';}
+si void scan(string &s) {for (char c; !isspace(c = gcu()); s += c);}
+#define pcu putchar_unlocked
+#define svo si void out
+template <typename T>
+svo(T n) {
+	static char buf[20];
+	char *p = buf;
+	//if(n < 0) pcu('-'), n *= -1;
+	if (!n) *p++ = '0';
+	else while (n) *p++ = n % 10 + '0', n /= 10;
+	while (p != buf) pcu(*--p); }
+svo(const char *s){while(*s)pcu(*s++);}
+svo(char *s){while(*s)pcu(*s++);}
+svo(char c){pcu(c);}
+svo(string &s){for (char c: s) pcu(c);}
+//template <typename T>
+//svo(vector<T> &v){for(T &x:v)out(' '),out(x);out('\n');}
+//svo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+template <typename head, typename... tail>
+svo(head&& h, tail&&... t){out(h);out(move(t)...);}
+#undef svo
+#undef si
 
 struct KMP {
 	vector<int> pi;
 	string T, P;
 
 	KMP() {
-		cin >> T >> P;
+		scan(T);
+		scan(P);
 		prifix_function();
 	}
 
@@ -36,7 +69,7 @@ struct KMP {
 			if (P[q + 1] == T[i])
 				q++;
 			if (q == m - 1) {
-				cout << i - m + 1 << endl;
+				out(i - m + 1, '\n');
 				q = pi[q];
 			}
 		}
