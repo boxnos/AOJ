01509e9924420ee3a9ad07ef1e89a86f358b1d4c
boxnos <boxnos@yahoo.com>
2018-09-12 23:48:13 +0900

    Compression version for ALDS1_14_C

--- ALDS1/ALDS1_14_C/main.cpp
+++ ALDS1/ALDS1_14_C/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <cctype>
 #include <vector>
+#include <deque>
 using namespace std;
 
 #define gcu getchar_unlocked
@@ -39,6 +40,11 @@ struct rect_search {
 	vs T, P;
 	int H, W, R, C;
 
+	struct node {char c; int n;};
+	typedef deque<node> dn;
+	v<dn> G;
+	v<v<dn>> M;
+
 	static void read(int &r, int &c, vs &S) {
 		r = in();
 		c = in();
@@ -46,36 +52,93 @@ struct rect_search {
 		for (auto &s: S)
 			scan(s);
 	}
+
 	rect_search() {
 		read(H, W, T);
 		read(R, C, P);
+
+		G = v<dn>(R);
+		for (int i = 0; i < R; i++)
+			G[i] = pack(P[i]);
+		M = v<v<dn>>(H, v<dn>(W, dn(0)));
+	}
+
+	static dn pack(string s) {
+		dn n;
+		n.push_back({s[0], 1});
+
+		for (size_t i = 1, j = 0; i < s.size(); i++)
+			if (s[i] == n[j].c)
+				n[j].n++;
+			else {
+				n.push_back({s[i], 1});
+				j++;
+			}
+		return n;
+	}
+
+	bool cmp(int r, int c, dn &b) {
+		if (M[r][c].size())
+			return M[r][c] == b;
+		dn a;
+		if (c == 0)
+			a = pack(T[r].substr(c, C));
+		else {
+			if (!M[r][c - 1].size())
+				cmp(r, c - 1, b);
+			a = M[r][c - 1];
+			node n = a.front();
+			a.pop_front();
+			if (--n.n)
+				a.push_front(n);
+			if (a.size() && a.back().c == T[r][c + C - 1])
+				a.back().n++;
+			else
+				a.push_back({T[r][c + C - 1], 1});
+		}
+		M[r][c] = a;
+		return a == b;
 	}
 
 	void search () {
-		for (int i = 0; i <= H - R; i++)
+		for (int i = 0; i <= H - R; i++) {
 			for (int j = 0; j <= W - C; j++) {
 				int k = 0;
 				for (; k < R; k++)
-					if (P[k] != T[i + k].substr(j, C))
+					if (!cmp(i + k, j, G[k]))
 						break;
 				if (k == R)
 					out(i, ' ', j, '\n');
 			}
-
+		}
 	}
 
+	void debug(dn d) {
+		for (auto n: d)
+			out(n.c, "[", n.n ,"] ");
+		out('\n');
+	}
 	void print() {
 		for (auto r: T)
 			out(r, '\n');
 		for (auto r: P)
 			out(r, '\n');
+		for (auto r: G) {
+			for (auto n: r)
+				out(n.c, "[", n.n ,"] ");
+			out('\n');
+		}
 	}
 };
 
+bool operator == (const rect_search::node &a, const rect_search::node &b) {
+	return a.c == b.c && a.n == b.n;
+}
+
 int main() {
 	rect_search rs = rect_search();
+	//rs.print();
 	rs.search();
-	// rs.print();
 }
 
 /* vim: set ts=4 noet: */
