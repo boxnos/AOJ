98a8967f37a7c0132b32417218b9744226314f1b
boxnos <boxnos@yahoo.com>
2018-09-15 19:47:44 +0900

    Accept 1129 in C++11

---
+++ 011/1129/main.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <utility>
+#include <deque>
+using namespace std;
+
+#define gcu getchar_unlocked
+#define ii inline int in
+ii(int c) {
+	int n = 0;
+	bool minus = false; if (c == '-') minus = true, c = gcu();
+	do {n = 10 * n + (c - '0'), c = gcu();} while (c >= '0');
+	//return n; }
+	return minus ? -n : n; }
+ii() {return in(gcu());}
+//void scan(string &s) {for (char c; !isspace(c = gcu()); s += c);}
+inline bool scan(int &n) {int c=gcu();return c==EOF?false:(n=in(c),true);}
+#undef ii
+#define pcu putchar_unlocked
+template <typename T>
+void out(T n) {
+	static char buf[20];
+	char *p = buf;
+	if(n < 0) pcu('-'), n *= -1;
+	if (!n) *p++ = '0';
+	else while (n) *p++ = n % 10 + '0', n /= 10;
+	while (p != buf) pcu(*--p); }
+void out(const char *s){while(*s)pcu(*s++);}
+void out(char c){pcu(c);}
+//void out(string &s){for (char c: s) pcu(c);}
+template <typename head, typename... tail>
+void out(head&& h, tail&&... t){out(h);out(move(t)...);}
+//template <typename T>
+//void out(vector<T> &v){for(T &x:v)out(' '),out(x);out('\n');}
+//void out(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+#undef svo
+
+int main() {
+	int n, r;
+	while (scan(n) && scan(r) && (n || r)) {
+		deque<int> deck = deque<int>(n);
+		for (int &d: deck)
+			d = n--;
+		while (r--) {
+			int p = in(), c = in();
+			for (int i = 0; i < c; i++) {
+				int tmp = deck[p + c - 2];
+				deck.erase(deck.begin() + p + c - 2);
+				deck.push_front(tmp);
+			}
+		}
+		out(deck[0], '\n');
+	}
+}
+
+/* vim: set ts=4 noet: */
