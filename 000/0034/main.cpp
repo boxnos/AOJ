7961ea2d7f5f3ab42ac174ca4f7c0dff43aa8a12
boxnos <boxnos@yahoo.com>
2018-10-02 04:20:05 +0900

    Accept 0034 in C++11

---
+++ 000/0034/main.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <utility>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define gcu getchar_unlocked
+#define ii inline int in
+#define is inline bool scan
+ii(int c) {
+	int n = 0;
+	bool minus = false; if (c == '-') minus = true, c = gcu();
+	do {n = 10 * n + (c - '0'), c = gcu();} while (c >= '0');
+	//return n; }
+	return minus ? -n : n; }
+ii() {return in(gcu());}
+is(int &n) {int c=gcu();return c==EOF?false:(n=in(c),true);}
+is(char &c){c=gcu();gcu();return c!=EOF;}
+//is(string &s) {int c;for(;;){c=gcu();if(c=='\n'||c==' ')return true;s+=c;}}
+#undef ii
+#define pcu putchar_unlocked
+template <typename T>
+void out(T n) {
+	static char buf[20];
+	char *p = buf;
+	if(n < 0) pcu('-'), n *= -1;
+	if (!n) *p++ = '0';
+	else while (n) *p++ = n % 10 + '0', n /= 10;
+	while (p != buf) pcu(*--p); }
+void out(const char *s){while(*s)pcu(*s++);}
+void out(char c){pcu(c);}
+//void out(string &s){for (char c: s) pcu(c);}
+template <typename head, typename... tail>
+void out(head&& h, tail&&... t){out(h);out(move(t)...);}
+template <typename T>
+//void out(vector<T> &v){for(T &x:v)out(' '),out(x);out('\n');}
+void out(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+#undef svo
+
+int main() {
+	vector<int> l(10);
+	while (scan(l[0])) {
+		int sum = l[0];
+		for_each(l.begin() + 1, l.end(), [&](int &n) {sum += in(), n = sum;});
+		int v1 = in(), vl = in() + v1, pos = v1 * sum;
+		int i = 1;
+		for (int n: l) {
+			if (pos - n * vl <= 0) {
+				out(i);
+				break;
+			}
+			i++;
+		}
+		out('\n');
+	}
+}
+
+/* vim: set ts=4 noet: */
