15ff38321446d857a4e569b41291c702ae4d8e52
boxnos <boxnos@yahoo.com>
2018-11-17 16:11:28 +0900

    Accept 2252

---
+++ 022/2252/main.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <utility>
+#include <string>
+using namespace std;
+
+#define gcu getchar_unlocked
+int in(int c){int n=0;bool m=false;if(c=='-')m=true,c=gcu();
+	do{n=10*n+(c-'0'),c=gcu();}while(c>='0');return m?-n:n;}
+int in() {return in(gcu());}
+bool scan(int &n){int c=gcu();return c==EOF?false:(n=in(c),true);}
+bool scan(char &c){c=gcu();gcu();return c!=EOF;}
+bool scan(string &s){int c;s="";
+	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
+#define pcu putchar_unlocked
+#define vo inline void out
+template <typename T>
+vo(T n){static char buf[20];char *p=buf;
+	if(n<0)pcu('-'),n*=-1;if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10;
+	while (p!=buf)pcu(*--p);}
+vo(const char *s){while(*s)pcu(*s++);}
+vo(char c){pcu(c);}
+vo(string &s){for (char c: s) pcu(c);}
+template <typename head, typename... tail> vo(head&& h, tail&&... t){out(h);out(move(t)...);}
+//template <typename T> vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+#undef vo
+
+int main() {
+	bool t['z' + 1] = {};
+	for (int c: "yuiophjklnm")
+		t[c] = true;
+	for (string s; scan(s) && s != "#";) {
+		int n = -1, h = -1;
+		for (int c: s) {
+			if (t[c] != h)
+				n++;
+			h = t[c];
+		}
+		out(n, '\n');
+	}
+}
+
+/* vim: set ts=4 noet: */
