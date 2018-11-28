195a13b95f2280fac5f0f01ff3204be63ae147d0
boxnos <boxnos@yahoo.com>
2018-11-28 18:50:42 +0900

    Refactor 1186

--- 011/1186/main.cpp
+++ 011/1186/main.cpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <vector>
 #include <algorithm>
+#include <tuple>
 using namespace std;
 
 #define gcu getchar_unlocked
@@ -27,22 +28,18 @@ template <typename head, typename... tail> vo(head&& h, tail&&... t){out(h);out(
 //template <typename T> vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
 #undef vo
 
-struct P {
-	int w, h, d;
-	bool operator < (const P &p) const {
-		 return d == p.d ? w < p.w : d < p.d;
-	}
-};
+typedef tuple<int, int, int> P;
+enum {D, H, W, N = 150};
 
 int main() {
 	vector<P> p;
-	for (int i = 1; i < 150; i++)
-		for (int j = i + 1; j < 150; j++)
-			p.push_back({i, j, i * i + j * j});
+	for (int i = 1; i < N; i++)
+		for (int j = i + 1; j < N; j++)
+			p.push_back({i * i + j * j, i, j});
 	sort(p.begin(), p.end());
 	for (int w, h; w = in(), h = in(), w || h;) {
-		auto r = upper_bound(p.begin(), p.end(), P{w, h, w * w + h * h});
-		out((*r).w, ' ', (*r).h, '\n');
+		auto r = upper_bound(p.begin(), p.end(), P{w * w + h * h, w, h});
+		out(get<H>(*r), ' ', get<W>(*r), '\n');
 	}
 }
 
