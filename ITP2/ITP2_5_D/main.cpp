e67347acb08d5329887da04b309649472aadf6ab
boxnos <boxnos@yahoo.com>
2019-01-25 23:31:33 +0900

    Refactor I/O lib

--- ITP2/ITP2_5_D/main.cpp
+++ ITP2/ITP2_5_D/main.cpp
@@ -22,17 +22,17 @@ _sc(char &c){c=gcu();gcu();return c!=EOF;}
 template <typename H,typename... T> _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
 #define _vo _il void out
 #define _vl _il void outl
-template <typename T>
-_vo(T n){static char buf[20];char *p=buf;
-	if(n<0)pcu('-'),n*=-1;if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10;
-	while (p!=buf)pcu(*--p);}
 _vo(const char *s){while(*s)pcu(*s++);}
 _vo(char c){pcu(c);}
 //_vo(string &s){for(char c:s)pcu(c);}
 //_vo(string s){for(char c:s)pcu(c);}
-template <typename H,typename... T> _vo(H&& h, T&&... t){out(h);out(move(t)...);}
-template <typename T> _vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);out('\n');}
+template <typename T>
+_vo(T n){static char buf[20];char *p=buf;
+	if(n<0)pcu('-'),n*=-1;if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10;
+	while (p!=buf)pcu(*--p);}
 _vl(){out('\n');}
+template <typename T> _vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+template <typename H,typename... T> _vo(H&& h, T&&... t){out(h);out(move(t)...);}
 template <typename... T> _vl(T&&... t){out(move(t)...);outl();}
 
 int main() {
