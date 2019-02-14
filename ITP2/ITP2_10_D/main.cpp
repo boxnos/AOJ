ade0ebc2dc9799debc2f59c90ec1696591af0b85
boxnos <boxnos@yahoo.com>
2019-02-14 21:30:48 +0900

    Accept ITP2_10_D

---
+++ ITP2/ITP2_10_D/main.cpp
@@ -0,0 +1,84 @@
+#include <cstdio>
+#include <utility>
+#include <bitset>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define gcu getchar_unlocked
+#define pcu putchar_unlocked
+#define _T template <typename T>
+#define _HT template <typename H, typename... T>
+#define _il inline
+#define _in _il int in
+#define _sc _il bool scan
+_T T in(int c){T n=0;bool m=false;if(c=='-')m=true,c=gcu();
+	do{n=10*n+(c-'0'),c=gcu();}while(c>='0');return m?-n:n;} //&&c<='9'
+_in() {return in<int>(gcu());}
+_T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
+_sc(char &c){c=gcu();gcu();return c!=EOF;}
+#ifdef _GLIBCXX_STRING
+_sc(string &s){int c;s="";
+	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
+#endif
+_HT _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
+#define _vo _il void out
+#define _vl _il void outl
+_vo(const char *s){while(*s)pcu(*s++);}
+_vo(char c){pcu(c);}
+#ifdef _GLIBCXX_STRING
+_vo(string s){for(char c:s)pcu(c);}
+#endif
+_T _vo(T n){static char buf[20];char *p=buf;
+	if(n<0)pcu('-'),n*=-1;if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10;
+	while (p!=buf)pcu(*--p);}
+_vl(){out('\n');}
+#ifdef _GLIBCXX_VECTOR
+_T _vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+#endif
+_HT _vo(H&& h, T&&... t){out(h);out(move(t)...);}
+template <typename... T> _vl(T&&... t){out(move(t)...);outl();}
+
+int main() {
+	bitset<64> f = 0;
+	vector<bitset<64>> M(in());
+	for (auto &b: M)
+		for (int k = in(); k; k--)
+			b.set(in());
+	for (int q = in(); q; q--) {
+		int o = in(), m = in();
+		switch (o) {
+		case 0:
+			outl((int) f.test(m));
+			break;
+		case 1:
+			f |= M[m];
+			break;
+		case 2:
+			f &= ~M[m];
+			break;
+		case 3:
+			f ^= M[m];
+			break;
+		case 4:
+			outl((int) ((f & M[m] | ~M[m]).all()));
+			break;
+		case 5:
+			outl((int) (f & M[m]).any());
+			break;
+		case 6:
+			outl((int) (f & M[m]).none());
+			break;
+		case 7:
+			outl((f & M[m]).count());
+			break;
+		case 8:
+			outl((f & M[m]).to_ulong());
+			break;
+		default:
+			outl("error");
+		}
+	}
+}
+
+/* vim: set ts=4 noet: */
