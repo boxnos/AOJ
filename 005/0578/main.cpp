9ba9bed51a2b1bd4cd2135b28ffbcd5f9205cec4
boxnos <boxnos@yahoo.com>
2019-05-15 21:07:00 +0900

    Accept 0578

---
+++ 005/0578/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+#include <cctype>
+#include <utility>
+#include <string>
+using namespace std;
+
+const auto gcu = getchar_unlocked;
+const auto pcu = putchar_unlocked;
+#define _T template <typename T>
+#define _HT template <typename H, typename... T>
+#define _il inline
+#define _in _il int in
+#define _sc _il bool scan
+_T T in(int c){T n=0;bool m=false;while(isspace(c)){c=gcu();}if(c=='-')m=true,c=gcu();
+	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m?-n:n;}
+_in() {return in<int>(gcu());}
+_T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
+_sc(char &c){c=gcu();gcu();return c!=EOF;}
+#ifdef _GLIBCXX_STRING
+_sc(string &s){int c;s="";
+	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
+#endif
+_HT _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
+#define _vo _il void out
+#define _vl _il void outl
+_vo(bool b) {pcu('0'+b);}
+_vo(const char *s){while(*s)pcu(*s++);}
+_vo(char c){pcu(c);}
+#ifdef _GLIBCXX_STRING
+_vo(string s){for(char c:s)pcu(c);}
+#endif
+_T _vo(T n){static char buf[20];char *p=buf;if(n<0)pcu('-'),n*=-1;
+	if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10; while (p!=buf)pcu(*--p);}
+_vl(){out('\n');}
+#ifdef _GLIBCXX_VECTOR
+_T _vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+#endif
+_HT _vo(H&& h, T&&... t){out(h);out(move(t)...);}
+template <typename... T> _vl(T&&... t){out(move(t)...);outl();}
+
+int main() {
+	int n = in(), c = 0;
+	string s, t;
+	scan(s);
+	while (n--) {
+		scan(t);
+		c += [&] {
+			for (int i = 0; i <= (int) t.size() - (int) s.size(); i++)
+				if (s[0] == t[i])
+					for (int j = 1; i + j * (s.size() - 1) < t.size(); j++) {
+						size_t k = 1;
+						for (; k < s.size() ; k++)
+							if (t[i + j * k] != s[k])
+								break;
+						if (k == s.size())
+							return 1;
+					}
+			return 0;
+		}();
+	}
+	outl(c);
+}
+
+/* vim: set ts=4 noet: */
