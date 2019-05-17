820a252eb0dff6d219f9c2428e53104de2eafd75
boxnos <boxnos@yahoo.com>
2019-05-17 21:32:24 +0900

    Refactor 0174
    - Rename I/O lib
    - Fix rvalue increment

--- 001/0174/main.cpp
+++ 001/0174/main.cpp
@@ -22,29 +22,29 @@ _sc(string &s){int c;s="";
 	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
 #endif
 _HT _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
-#define _vo _il void out
-#define _vl _il void outl
-_vo(bool b) {pcu('0'+b);}
-_vo(const char *s){while(*s)pcu(*s++);}
-_vo(char c){pcu(c);}
+#define _ot _il void out
+#define _ol _il void outl
+_ot(bool b) {pcu('0'+b);}
+_ot(const char *s){while(*s)pcu(*s++);}
+_ot(char c){pcu(c);}
 #ifdef _GLIBCXX_STRING
-_vo(string s){for(char c:s)pcu(c);}
+_ot(string s){for(char c:s)pcu(c);}
 #endif
-_T _vo(T n){static char buf[20];char *p=buf;if(n<0)pcu('-'),n*=-1;
+_T _ot(T n){static char buf[20];char *p=buf;if(n<0)pcu('-'),n*=-1;
 	if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10; while (p!=buf)pcu(*--p);}
-_vl(){out('\n');}
+_ol(){out('\n');}
 #ifdef _GLIBCXX_VECTOR
-_T _vo(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+_T _ot(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
 #endif
-_HT _vo(H&& h, T&&... t){out(h);out(move(t)...);}
-template <typename... T> _vl(T&&... t){out(move(t)...);outl();}
+_HT _ot(H&& h, T&&... t){out(h);out(move(t)...);}
+template <typename... T> _ol(T&&... t){out(move(t)...);outl();}
 
 int main() {
 	for (char c; c = gcu(), c != '0';) {
 		string s; scan(s);
 		int A = count(s.begin(), s.end(), 'A'),
 			B = s.size() - A;
-		(*(A > B ? &A : &B))++;
+		(A > B ? A : B)++;
 		outl(A, ' ', B);
 	}
 }
