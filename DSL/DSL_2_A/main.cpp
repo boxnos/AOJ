a3fcce1361b4248b8f34c75650efaee495f9bb60
boxnos <boxnos@yahoo.com>
2019-05-22 00:11:09 +0900

    Fix I/O lib bug
    Allow out(INT_MIN)

--- DSL/DSL_2_A/main.cpp
+++ DSL/DSL_2_A/main.cpp
@@ -10,11 +10,10 @@ const auto pcu = putchar_unlocked;
 #define _T template <typename T>
 #define _HT template <typename H, typename... T>
 #define _il inline
-#define _in _il int in
 #define _sc _il bool scan
 _T T in(int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
 	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
-_in() {return in<int>(gcu());}
+_il int in() {return in<int>(gcu());}
 _T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
 _sc(char &c){c=gcu();gcu();return c!=EOF;}
 #ifdef _GLIBCXX_STRING
@@ -30,8 +29,8 @@ _ot(char c){pcu(c);}
 #ifdef _GLIBCXX_STRING
 _ot(string s){for(char c:s)pcu(c);}
 #endif
-_T _ot(T n){static char buf[20];char *p=buf;if(n<0)pcu('-'),n*=-1;
-	if(!n)*p++='0';else while(n)*p++=n%10+'0',n/=10; while (p!=buf)pcu(*--p);}
+_T _ot(T n){static char b[20];char *p=b,m=n<0?pcu('-'),-1:1;
+	if(!n)*p++='0';else while(n)*p++=n%10*m+'0',n/=10;while(p!=b)pcu(*--p);}
 _ol(){out('\n');}
 #ifdef _GLIBCXX_VECTOR
 _T _ot(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
