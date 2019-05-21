6fabae9c47f86c33aeed09716db245abf5ab7abd
boxnos <boxnos@yahoo.com>
2019-05-21 18:18:36 +0900

    Optimize DSL_1_A
    Use segment tree

--- DSL/DSL_2_A/main.cpp
+++ DSL/DSL_2_A/main.cpp
@@ -3,7 +3,6 @@
 #include <utility>
 #include <vector>
 #include <climits>
-#include <algorithm>
 using namespace std;
 
 const auto gcu = getchar_unlocked;
@@ -13,8 +12,8 @@ const auto pcu = putchar_unlocked;
 #define _il inline
 #define _in _il int in
 #define _sc _il bool scan
-_T T in(int c){T n=0;bool m=false;while(isspace(c)){c=gcu();}if(c=='-')m=true,c=gcu();
-	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m?-n:n;}
+_T T in(int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
+	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
 _in() {return in<int>(gcu());}
 _T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
 _sc(char &c){c=gcu();gcu();return c!=EOF;}
@@ -40,15 +39,40 @@ _T _ot(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
 _HT _ot(H&& h, T&&... t){out(h);out(move(t)...);}
 template <typename... T> _ol(T&&... t){out(move(t)...);outl();}
 
+struct RMQ {
+	int n;
+	vector<int> t;
+	RMQ (int l) :
+		n(1 << (32 - __builtin_clz(l - 1))),
+		t(n * 2 - 1, INT_MAX) {}
+	void update(int i, int v) {
+		i += n - 1, t[i] = v;
+		while (i) {
+			i = (i - 1) / 2;
+			t[i] = min(t[i * 2 + 1], t[i * 2 + 2]);
+		}
+	}
+	int find(int a, int b, int k, int l, int r) {
+		if (r <= a || b <= l)
+			return INT_MAX;
+		if (a <= l && r <= b)
+			return t[k];
+		int m = (l + r) / 2;
+		k *= 2;
+		return min(find(a, b, k + 1, l, m),
+				   find(a, b, k + 2, m, r));
+	}
+};
+
 int main() {
-	vector<int> s(in(), INT_MAX);
+	RMQ r(in());
 	for (int q = in(); q; q--) {
 		if (in()) {
-			int x = in();
-			outl(*min_element(s.begin() + x, s.begin() + in() + 1));
+			int x = in(), y = in();
+			outl(r.find(x, y + 1, 0, 0, r.n));
 		} else {
-			int x = in();
-			s[x] = in();
+			int x = in(), y = in();
+			r.update(x, y);
 		}
 	}
 }
