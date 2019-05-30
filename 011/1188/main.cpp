02b6280998295e15d35501ab9af2d8d6e78f1c96
boxnos <boxnos@yahoo.com>
2019-05-30 20:39:23 +0900

    Accept 1188

---
+++ 011/1188/main.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <cctype>
+#include <utility>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+const auto gcu = getchar_unlocked;
+const auto pcu = putchar_unlocked;
+#define _T template <typename T>
+#define _HT template <typename H, typename... T>
+#define _il inline
+#define _sc _il bool scan
+_T T in(int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
+	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+_il int in() {return in<int>(gcu());}
+_T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
+_sc(char &c){c=gcu();gcu();return c!=EOF;}
+#ifdef _GLIBCXX_STRING
+_sc(string &s){int c;s="";
+	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
+#endif
+_HT _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
+#define _ot _il void out
+#define _ol _il void outl
+_ot(bool b) {pcu('0'+b);}
+_ot(const char *s){while(*s)pcu(*s++);}
+_ot(char c){pcu(c);}
+#ifdef _GLIBCXX_STRING
+_ot(string s){for(char c:s)pcu(c);}
+#endif
+_T _ot(T n){static char b[20];char *p=b,m=n<0?pcu('-'),-1:1;
+	if(!n)*p++='0';else while(n)*p++=n%10*m+'0',n/=10;while(p!=b)pcu(*--p);}
+_ol(){out('\n');}
+#ifdef _GLIBCXX_VECTOR
+_T _ot(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+#endif
+_HT _ot(H&& h, T&&... t){out(h);out(move(t)...);}
+template <typename... T> _ol(T&&... t){out(move(t)...);outl();}
+
+int elect() {
+	int c = gcu();
+	switch(c) {
+	case '[':
+		{
+		vector<int> v;
+		for (int r; (r = elect());)
+			v.push_back(r);
+		auto m = v.begin() + v.size() / 2 + 1;
+		nth_element(v.begin(), m, v.end());
+		return accumulate(v.begin(), m, 0);
+		}
+	case ']':
+		return 0;
+	default:
+		ungetc(c, stdin);
+		scanf("%d", &c);
+		return c / 2 + 1;
+	}
+}
+
+int main() {
+	for (int n = in(); n--; gcu())
+		outl(elect());
+}
+
+/* vim: set ts=4 noet: */
