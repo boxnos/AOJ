1714aea68be766c2b09297c3244ce0d07ea7411d
boxnos <boxnos@yahoo.com>
2019-06-01 00:24:34 +0900

    Update I/O lib

--- 005/0529/main.cpp
+++ 005/0529/main.cpp
@@ -9,36 +9,36 @@ using namespace std;
 
 const auto gcu = getchar_unlocked;
 const auto pcu = putchar_unlocked;
+#define _DEF(r, n, ...) inline r n(__VA_ARGS__) noexcept
 #define _T template <typename T>
-#define _HT template <typename H, typename... T>
-#define _il inline
-#define _sc _il bool scan
-_T T in(int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
+#define _HT template <typename H,typename... T>
+_T _DEF(T,in,int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
 	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
-_il int in() {return in<int>(gcu());}
-_T T scan(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
-_sc(char &c){c=gcu();gcu();return c!=EOF;}
+_DEF(int, in){return in<int>(gcu());}
+#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
+_T _SCAN(T &n){int c=gcu();return c==EOF?false:(n=in<T>(c),true);}
+_DEF(bool, scan, char &c){c=gcu();gcu();return c!=EOF;}
 #ifdef _GLIBCXX_STRING
-_sc(string &s){int c;s="";
+_SCAN(string &s){int c;s="";
 	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=c;}}
 #endif
-_HT _sc(H &h, T&&... t){return scan(h)&&scan(t...);}
-#define _ot _il void out
-#define _ol _il void outl
-_ot(bool b) {pcu('0'+b);}
-_ot(const char *s){while(*s)pcu(*s++);}
-_ot(char c){pcu(c);}
+_HT _SCAN(H &h,T&&... t){return scan(h)&&scan(t...);}
+#define _OUT(...) _DEF(void,out,__VA_ARGS__)
+#define _OUTL(...) _DEF(void,outl,__VA_ARGS__)
+_OUT(bool b){pcu('0'+b);}
+_OUT(const char *s){while(*s)pcu(*s++);}
+_OUT(char c){pcu(c);}
 #ifdef _GLIBCXX_STRING
-_ot(string s){for(char c:s)pcu(c);}
+_OUT(string s){for(char c:s)pcu(c);}
 #endif
-_T _ot(T n){static char b[20];char *p=b,m=n<0?pcu('-'),-1:1;
+_T _OUT(T n){static char b[20];char *p=b,m=n<0?pcu('-'),-1:1;
 	if(!n)*p++='0';else while(n)*p++=n%10*m+'0',n/=10;while(p!=b)pcu(*--p);}
-_ol(){out('\n');}
+_OUTL(){out('\n');}
 #ifdef _GLIBCXX_VECTOR
-_T _ot(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
+_T _OUT(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);outl();}
 #endif
-_HT _ot(H&& h, T&&... t){out(h);out(move(t)...);}
-template <typename... T> _ol(T&&... t){out(move(t)...);outl();}
+_HT _OUT(H&& h, T&&... t){out(h);out(move(t)...);}
+template <typename... T> _OUTL(T&&... t){out(move(t)...);outl();}
 
 #define all(o) o.begin(), o.end()
 
