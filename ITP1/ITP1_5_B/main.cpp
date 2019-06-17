810f89be6d8810ced9303f1ebfbe343a56c104dd
boxnos <boxnos@yahoo.com>
2019-06-17 14:28:22 +0900

    Refactor ITP1_5_B

--- ITP1/ITP1_5_B/main.cpp
+++ ITP1/ITP1_5_B/main.cpp
@@ -1,18 +1,13 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main()
-{
-    int H, W;
-
-    while (cin >> H >> W && H && W) {
-        cout << string(W, '#') << endl;
-        for (int i = 2; i < H; i++)
-            cout << '#' << string(W - 2, '.') << '#' << endl;
-        cout << string(W, '#') << endl;
+int main() {
+    for (int H, W; cin >> H >> W, W;) {
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++)
+                cout << (i != H - 1 && j != W - 1 && i && j ? '.' : '#');
+            cout << endl;
+        }
         cout << endl;
     }
-
-    return 0;
 }
