92a4b94eb8994102eb6ac131e0a1ef8c0e3b5d7f
boxnos <boxnos@yahoo.com>
2019-11-01 22:42:45 +0900

    Accept 0212

---
+++ 002/0212/main.cpp
@@ -0,0 +1,107 @@
+#include <cstdio>
+#include <utility>
+#include <cctype>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <climits>
+using namespace std;
+
+#ifdef __linux
+#define _U(s) s##_unlocked
+#else
+#define _U(s) _##s##_nolock
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#endif
+#define gcu _U(getchar)
+#define pcu _U(putchar)
+#define _DEF(r, n, ...) inline r n(__VA_ARGS__) noexcept
+#define _T template <typename T>
+#define _HT template <typename H,typename... T>
+#define _OP(t) inline operator t()
+struct _in {
+#ifdef _GLIBCXX_STRING
+	_OP(string){string s;char c;while(isspace(c = gcu()));do{s+=c;}while(c=gcu(),c!=' '&&c!='\n'&&c!=EOF);return s;}
+#define _S
+#endif
+	_OP(char){char c=gcu();gcu();return c;}
+	_OP(double){double d; scanf("%lf",&d); gcu();return d;}
+	_T _OP(T){T n{},m{1},c;while(isspace(c = gcu()));if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+} in;
+#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
+#ifdef _S
+_SCAN(string &o) {int c{gcu()};if(c==EOF)return false;else{ungetc(c,stdin);string t=move(in);o=t;return true;}}
+#endif
+_T _SCAN(T &o) {int c{gcu()};return c==EOF?false:(ungetc(c,stdin),o=in,true);}
+_HT _SCAN(H &h,T&&... t){return scan(h)&&scan(t...);}
+#define _OUT(...) _DEF(void,out,__VA_ARGS__)
+#define _OUTL(...) _DEF(void,outl,__VA_ARGS__)
+_OUT(bool b){pcu('0'+b);}
+_OUT(const char *s){while(*s)pcu(*s++);}
+_OUT(char c){pcu(c);}
+#ifdef _S
+_OUT(string &s){for(char c:s)pcu(c);}
+#endif
+_T _OUT(T n){static char b[20];char *p=b;T m=n<0?pcu('-'),-1:1;
+	if(!n)*p++='0';else while(n)*p++=(char)(n%10*m+'0'),n/=10;while(p!=b)pcu(*--p);}
+_OUTL(){out('\n');}
+#ifdef _GLIBCXX_VECTOR
+	_T _OUT(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
+#endif
+	_HT _OUT(H &&h, T... t){out(h);out(t...);}
+	template <typename... T> _OUTL(T... t){out(t...);outl();}
+	struct range{
+		int e,b=0,s=1;range(int _b,int _e,int _s):e(_e),b(_b),s(_s){} range(int _b,int _e): e(_e), b(_b){} range(int _e):e(_e){}
+		struct it{int v,s; it(int _v,int _s):v(_v),s(_s){} operator int()const{return v;} operator int&(){return v;}int operator*()const{return v;}
+			it& operator++(){v+=s;return *this;} }; it begin(){return {b,s};} it end(){return {e,s};}};
+#define times(i,n) for(int i=n;i;i--)
+
+template <typename T>
+using V = vector<T>;
+
+struct E {
+	int t, c;
+};
+
+struct P {
+	int c, t, k;
+	bool operator < (P a) const {return c > a.c;}
+};
+
+int main() {
+	for (int c, n, m, s, d; c = in, n = in, m = in, s = in, d = in;) {
+		s--, d--;
+		V<V<E>> v(n, V<E>());
+		times(i, m) {
+			int a {(int) in - 1}, b {(int) in - 1}, f {in};
+			v[a].push_back({b, f});
+			v[b].push_back({a, f});
+		}
+		V<V<int>> dp(c + 1, V<int>(n, INT_MAX));
+		dp[c][s] = 0;
+		priority_queue<P> q;
+		q.push({0, s, c});
+		while (!q.empty()) {
+			P t = q.top();
+			q.pop();
+			if (dp[t.k][t.t] < t.c)
+				continue;
+			for (E e: v[t.t]) {
+				if (t.k > 0 && dp[t.k - 1][e.t] > dp[t.k][t.t] + e.c / 2) {
+					dp[t.k - 1][e.t] = dp[t.k][t.t] + e.c / 2;
+					q.push({dp[t.k - 1][e.t], e.t, t.k - 1});
+				}
+				if (dp[t.k][e.t] > dp[t.k][t.t] + e.c) {
+					dp[t.k][e.t] = dp[t.k][t.t] + e.c;
+					q.push({dp[t.k][e.t], e.t, t.k});
+				}
+			}
+		}
+		int r {INT_MAX};
+		for (auto i: dp)
+			r = min(r, i[d]);
+		outl(r);
+	}
+}
+
+/* vim: set ts=4 noet: */
